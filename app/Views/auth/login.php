cfaca1c28d19b3d4834d335a6e5c6911a740b2e3
leonardo-wjy <leo.wijaya.0728@gmail.com>
2023-04-06 14:03:49 +0700

    url fix

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -7,12 +7,12 @@
     <title><?= getenv('pageTitle') ?></title>
 
     <!-- General CSS Files -->
-    <link rel="stylesheet" href="<?= base_url() ?>/template/node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>template/node_modules/bootstrap/dist/css/bootstrap.min.css">
 
     <!-- CSS Libraries -->
 
     <!-- Template CSS -->
-    <link rel="stylesheet" href="<?= base_url() ?>/template/assets/css/style.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>template/assets/css/style.css">
 </head>
 
 <body>
@@ -22,7 +22,7 @@
                 <div class="row">
                     <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
                         <div class="login-brand">
-                            <img src="<?= base_url() ?>/template/assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
+                            <img src="<?= base_url(); ?>template/assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
                         </div>
 
                         <div class="card card-primary">
@@ -82,16 +82,16 @@
     <!-- General JS Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="<?= base_url() ?>/template/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="<?= base_url(); ?>template/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-    <script src="<?= base_url() ?>/template/assets/js/stisla.js"></script>
+    <script src="<?= base_url(); ?>template/assets/js/stisla.js"></script>
 
     <!-- JS Libraies -->
 
     <!-- Template JS File -->
-    <script src="<?= base_url() ?>/template/assets/js/scripts.js"></script>
-    <script src="<?= base_url() ?>/template/assets/js/custom.js"></script>
+    <script src="<?= base_url(); ?>template/assets/js/scripts.js"></script>
+    <script src="<?= base_url(); ?>template/assets/js/custom.js"></script>
 
     <!-- Page Specific JS File -->
 </body>
