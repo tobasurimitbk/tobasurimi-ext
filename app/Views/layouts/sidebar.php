145d38f297c8bbb1fa0afdac2d326ebf4f0e09d6
leonardo-wjy <leo.wijaya.0728@gmail.com>
2023-04-11 15:16:15 +0700

    user

--- app/Views/layouts/sidebar.php
+++ app/Views/layouts/sidebar.php
@@ -1,3 +1,9 @@
+<?php
+$currentUriSegment = service('uri')->getSegment(1);
+$arrSettingURI = ['user'];
+$arrDashboardURI = ['dashboard'];
+?>
+
 <!-- Sidebar -->
 <div class="main-sidebar bg-dark sidebar-style-2">
     <aside id="sidebar-wrapper">
@@ -7,18 +13,22 @@
         </div>
 
         <ul class="sidebar-menu">
-            <li class="nav-item dropdown{{ ' active'|is_active('^index(.*)', page)|safe }}">
+            <li class="nav-item dropdown <?= (in_array($currentUriSegment, $arrDashboardURI) ? "active" : "{{ ' active'|is_active('^index(.*)', page)|safe }}") ?>">
                 <a href="<?= base_url("dashboard"); ?>" class="nav-link"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a>
             </li>
         </ul>
 
         <ul class="sidebar-menu">
-            <li class="nav-item dropdown{{ ' active'|is_active('^index(.*)', page)|safe }}">
+        <li class="nav-item dropdown <?= (in_array($currentUriSegment, $arrSettingURI) ? "active" : "{{ ' active'|is_active('^index(.*)', page)|safe }}") ?>">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-cog"></i><span>Settings</span></a>
                 <ul class="dropdown-menu">
+                    <?php if($currentUriSegment === "user") { ?>
+                    <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link" href="<?= base_url("user"); ?>">Manajemen User</a></li>
+                    <?php }else {?>
                     <li><a class="nav-link" href="<?= base_url("user"); ?>">Manajemen User</a></li>
+                    <?php } ?>
                     <li><a class="nav-link">Manajemen Role</a></li>
-                    <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link">Manajemen Hak Akses</a></li>
+                    <li><a class="nav-link">Manajemen Hak Akses</a></li>
                 </ul>
             </li>
         </ul>
