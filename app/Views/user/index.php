5ed97ac128d21e04fffadd6225d26d6201b6e01d
leonardo-wjy <leo.wijaya.0728@gmail.com>
2023-04-11 14:35:05 +0700

    user

--- app/Views/user/index.php
+++ app/Views/user/index.php
@@ -1,40 +1,6 @@
 <?= $this->extend('layouts/template'); ?>
 <?= $this->Section('content'); ?>
 
-<!-- Begin Page Content -->
-<div class="container-fluid">
-    <div class="mb-5">
-        <h4 style="padding-top: 6px; color: #3B4758;" class="m-0 font-weight-bold my-2">Management User</h4>
-        <button onclick="showAddForm()" class="btn btn-add btn-block" data-btn="create-modal" style="width: 176px;">
-            <i class="fa fa-plus fa-sm mr-2" aria-hidden="true"></i>Create User
-        </button>
-   </div>
-   <div class="mb-2">
-        <h5 style="padding-top: 6px; color: #3B4758;" class="m-0 font-weight-bold my-2">= List User</h5>
-        <input type="text" class="form-control" placeholder="Search" style="width: 30%" />
-   </div>
-   <div class="table-responsive">
-        <table class="table table-bordered nowrap table-hover-pbtc" id="dataTable" width="100%" cellspacing="0">
-            <thead class="thead-dark">
-                <tr>
-                    <th>Username</th>
-                    <th>Name</th>
-                    <th>Role</th>
-                    <th>Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>johndoe</td>
-                    <td>John Doe</td>
-                    <td>Manager</td>
-                    <td>Aktif</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-</div>
-
 <div class="modal add-modal" tabindex="-1">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -54,13 +20,13 @@
                     <div class="col">
                         <div class="input-group input-group-password">
                             <div class="form-floating mb-3">
-                                <input type="text" class="form-control password" id="password" name="password" placeholder="Password" maxlength="30">
+                                <input type="password" class="form-control password" id="password" name="password" placeholder="Password" maxlength="30">
                                 <label for="floatingInput">Password</label>
                             </div>
                             <div class="input-group-prepend group-prepend-password align-items-center">
                                 <span style="border: 0px" class="input-group-text bg-white" id="basic-addon2" onclick="password_show_hide()">
-                                    <i class="fas fa-eye" id="show_eye"></i>
-                                    <i class="fas fa-eye-slash d-none" id="hide_eye"></i>
+                                    <i class="fas fa-eye d-none" id="show_eye"></i>
+                                    <i class="fas fa-eye-slash" id="hide_eye"></i>
                                 </span>
                             </div>
                         </div>
@@ -107,6 +73,40 @@
     </div>
 </div>
 
+<!-- Begin Page Content -->
+<div class="container-fluid">
+    <div class="mb-5">
+        <h4 style="padding-top: 6px; color: #3B4758;" class="m-0 font-weight-bold my-2">Management User</h4>
+        <button onclick="showAddForm()" class="btn btn-add btn-block" data-btn="create-modal" style="width: 176px;">
+            <i class="fa fa-plus fa-sm mr-2" aria-hidden="true"></i>Create User
+        </button>
+   </div>
+   <div class="mb-2">
+        <h5 style="padding-top: 6px; color: #3B4758;" class="m-0 font-weight-bold my-2">= List User</h5>
+        <input class="form-control" placeholder="Search" style="width: 30%" value="" />
+   </div>
+   <div class="table-responsive">
+        <table class="table table-bordered nowrap table-hover-pbtc" id="dataTable" width="100%" cellspacing="0">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Username</th>
+                    <th>Name</th>
+                    <th>Role</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>johndoe</td>
+                    <td>John Doe</td>
+                    <td>Manager</td>
+                    <td>Aktif</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
 <script>
     const showAddForm = function(e) {
         $(".add-modal").modal("show")
@@ -120,7 +120,7 @@
         var x = document.getElementById("password");
         var show_eye = document.getElementById("show_eye");
         var hide_eye = document.getElementById("hide_eye");
-        hide_eye.classList.remove("d-none");
+        show_eye.classList.remove("d-none");
         if (x.type === "text") {
             x.type = "password";
             show_eye.style.display = "none";
