7883dc107707f43b6e569590ea3de76f64b08e30
leonardo-wjy <leo.wijaya.0728@gmail.com>
2023-05-04 12:14:20 +0700

    time 24 hours

--- app/Views/divisi/index.php
+++ app/Views/divisi/index.php
@@ -33,37 +33,72 @@
                             </div>
                         </div>
                         <div class="col-md-6">
-                            <div class="form-floating mb-3" style="height: 50px;">
-                                <input type="time" class="form-control jam_istirahat" id="jam_istirahat" name="jam_istirahat">
-                                <label for="floatingInput">Jam Istirahat</label>
+                            <div class="input-group input-group-password">
+                                <div class="form-floating mb-3">
+                                    <input class="form-control input-time jam_istirahat" id="jam_istirahat" name="jam_istirahat">
+                                    <label for="floatingInput">Jam Istirahat</label>
+                                </div>
+                                <div class="input-group-prepend group-prepend-password align-items-center">
+                                    <span style="border: 0px" class="mt-3 input-group-text bg-white" id="basic-addon2">
+                                        <i class="fas fa-clock"></i>
+                                    </span>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div class="row mb-3">
                         <div class="col-md-6">
-                            <div class="form-floating mb-3" style="height: 50px;">
-                                <input type="time" class="form-control jam_masuk" id="jam_masuk" name="jam_masuk">
-                                <label for="floatingInput">Jam Masuk</label>
+                            <div class="input-group input-group-password">
+                                <div class="form-floating mb-3">
+                                    <input class="form-control input-time jam_masuk" id="jam_masuk" name="jam_masuk">
+                                    <label for="floatingInput">Jam Masuk</label>
+                                </div>
+                                <div class="input-group-prepend group-prepend-password align-items-center">
+                                    <span style="border: 0px" class="mt-3 input-group-text bg-white" id="basic-addon2">
+                                        <i class="fas fa-clock"></i>
+                                    </span>
+                                </div>
                             </div>
                         </div>
                         <div class="col-md-6">
-                            <div class="form-floating mb-3" style="height: 50px;">
-                                <input type="time" class="form-control jam_pulang" id="jam_pulang" name="jam_pulang">
-                                <label for="floatingInput">Jam Pulang</label>
+                            <div class="input-group input-group-password">
+                                <div class="form-floating mb-3">
+                                    <input class="form-control input-time jam_pulang" id="jam_pulang" name="jam_pulang">
+                                    <label for="floatingInput">Jam Keluar</label>
+                                </div>
+                                <div class="input-group-prepend group-prepend-password align-items-center">
+                                    <span style="border: 0px" class="mt-3 input-group-text bg-white" id="basic-addon2">
+                                        <i class="fas fa-clock"></i>
+                                    </span>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div class="row mb-3">
                         <div class="col-md-6">
-                            <div class="form-floating mb-3" style="height: 50px;">
-                                <input type="time" class="form-control mulai_istirahat" id="mulai_istirahat" name="mulai_istirahat">
-                                <label for="floatingInput">Jam Mulai Istirahat</label>
+                            <div class="input-group input-group-password">
+                                <div class="form-floating mb-3">
+                                    <input class="form-control input-time mulai_istirahat" id="mulai_istirahat" name="mulai_istirahat">
+                                    <label for="floatingInput">Jam Mulai Istirahat</label>
+                                </div>
+                                <div class="input-group-prepend group-prepend-password align-items-center">
+                                    <span style="border: 0px" class="mt-3 input-group-text bg-white" id="basic-addon2">
+                                        <i class="fas fa-clock"></i>
+                                    </span>
+                                </div>
                             </div>
                         </div>
                         <div class="col-md-6">
-                            <div class="form-floating mb-3" style="height: 50px;">
-                                <input type="time" class="form-control selesai_istirahat" id="selesai_istirahat" name="selesai_istirahat">
-                                <label for="floatingInput">Jam Selesai Istirahat</label>
+                            <div class="input-group input-group-password">
+                                <div class="form-floating mb-3">
+                                    <input class="form-control input-time selesai_istirahat" id="selesai_istirahat" name="selesai_istirahat">
+                                    <label for="floatingInput">Jam Selesai Istirahat</label>
+                                </div>
+                                <div class="input-group-prepend group-prepend-password align-items-center">
+                                    <span style="border: 0px" class="mt-3 input-group-text bg-white" id="basic-addon2">
+                                        <i class="fas fa-clock"></i>
+                                    </span>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -118,6 +153,19 @@
     const csrfToken = '<?= csrf_token() ?>';
     
     $(document).ready(function() {
+        $(".jam_istirahat, .jam_masuk, .jam_keluar, .mulai_istirahat, .selesai_istirahat").datetimepicker({
+            format: 'HH:mm',
+            useCurrent: false,
+            showTodayButton: false,
+            showClear: false,
+            toolbarPlacement: 'bottom',
+            sideBySide: true,
+            icons: {
+                up: "fa fa-arrow-up",
+                down: "fa fa-arrow-down"
+            }
+        });
+
         var validator = $(".create-form").validate({
             rules: {
                 divisi: {
--- app/Views/layouts/template.php
+++ app/Views/layouts/template.php
@@ -36,6 +36,9 @@
     <!-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"> -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.43/css/bootstrap-datetimepicker.min.css"> 
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.43/css/bootstrap-datetimepicker-standalone.css"> 
+    <script defer type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.43/js/bootstrap-datetimepicker.min.js"></script>
  </head>
 
 <body>
