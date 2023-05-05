b4ff48a0ea5703ca25b2aeb5091d0673a0987c8a
leonardo-wjy <leo.wijaya.0728@gmail.com>
2023-05-05 14:23:23 +0700

    sidebar menu css

--- app/Views/layouts/sidebar.php
+++ app/Views/layouts/sidebar.php
@@ -29,54 +29,54 @@ $arrDashboardURI = ['dashboard'];
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-database"></i><span>Master Data</span></a>
                 <ul class="dropdown-menu">
                     <?php if ($currentUriSegment === "customer") { ?>
-                        <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link" href="<?= base_url("customer"); ?>">Customer</a>
+                        <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link active-bar" href="<?= base_url("customer"); ?>">Customer</a>
             </li>
         <?php } else { ?>
             <li><a class="nav-link" href="<?= base_url("customer"); ?>">Customer</a></li>
         <?php } ?>
 
         <?php if ($currentUriSegment === "supplier") { ?>
-            <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link" href="<?= base_url("supplier"); ?>">Supplier</a>
+            <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link active-bar" href="<?= base_url("supplier"); ?>">Supplier</a>
                 </li>
             <?php } else { ?>
                 <li><a class="nav-link" href="<?= base_url("supplier"); ?>">Supplier</a></li>
             <?php } ?>
 
             <?php if ($currentUriSegment === "vendor") { ?>
-                <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link" href="<?= base_url("vendor"); ?>">Vendor</a>
+                <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link active-bar" href="<?= base_url("vendor"); ?>">Vendor</a>
                     </li>
                 <?php } else { ?>
                     <li><a class="nav-link" href="<?= base_url("vendor"); ?>">Vendor</a></li>
                 <?php } ?>
 
                 <?php if ($currentUriSegment === "produk-barang-jadi") { ?>
-                    <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link" href="<?= base_url("produk-barang-jadi"); ?>">Produk Barang Jadi</a>
+                    <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link active-bar" href="<?= base_url("produk-barang-jadi"); ?>">Produk Barang Jadi</a>
                         </li>
                     <?php } else { ?>
                         <li><a class="nav-link" href="<?= base_url("produk-barang-jadi"); ?>">Produk Barang Jadi</a></li>
                     <?php } ?>
 
                     <?php if ($currentUriSegment === "company") { ?>
-                        <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link" href="<?= base_url("company"); ?>">Company</a></li>
+                        <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link active-bar" href="<?= base_url("company"); ?>">Company</a></li>
                         <?php } else { ?>
                             <li><a class="nav-link" href="<?= base_url("company"); ?>">Company</a></li>
                         <?php } ?>
 
                         <?php if ($currentUriSegment === "warehouse") { ?>
-                            <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link" href="<?= base_url("warehouse"); ?>">Warehouse</a></li>
+                            <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link active-bar" href="<?= base_url("warehouse"); ?>">Warehouse</a></li>
                             <?php } else { ?>
                                 <li><a class="nav-link" href="<?= base_url("warehouse"); ?>">Warehouse</a></li>
                             <?php } ?>
 
                             <?php if ($currentUriSegment === "employee") { ?>
-                                <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link" href="<?= base_url("employee"); ?>">Employee</a>
+                                <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link active-bar" href="<?= base_url("employee"); ?>">Employee</a>
                                     </li>
                                 <?php } else { ?>
                                     <li><a class="nav-link" href="<?= base_url("employee"); ?>">Employee</a></li>
                                 <?php } ?>
 
                                 <?php if ($currentUriSegment === "divisi") { ?>
-                                    <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link" href="<?= base_url("divisi"); ?>">Divisi</a>
+                                    <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link active-bar" href="<?= base_url("divisi"); ?>">Divisi</a>
                                         </li>
                                     <?php } else { ?>
                                         <li><a class="nav-link" href="<?= base_url("divisi"); ?>">Divisi</a></li>
@@ -90,18 +90,18 @@ $arrDashboardURI = ['dashboard'];
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-cog"></i><span>Settings</span></a>
                 <ul class="dropdown-menu">
                     <?php if ($currentUriSegment === "user") { ?>
-                        <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link" href="<?= base_url("user"); ?>">Manajemen User</a>
+                        <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link active-bar" href="<?= base_url("user"); ?>">Manajemen User</a>
             </li>
         <?php } else { ?>
             <li><a class="nav-link" href="<?= base_url("user"); ?>">Manajemen User</a></li>
         <?php } ?>
         <?php if ($currentUriSegment === "role") { ?>
-            <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link" href="<?= base_url("role"); ?>">Manajemen Role</a></li>
+            <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link active-bar" href="<?= base_url("role"); ?>">Manajemen Role</a></li>
             <?php } else { ?>
                 <li><a class="nav-link" href="<?= base_url("role"); ?>">Manajemen Role</a></li>
             <?php } ?>
             <?php if ($currentUriSegment === "akses") { ?>
-                <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link" href="<?= base_url("akses"); ?>">Manajemen Hak Akses</a></li>
+                <li{{ ' class="active"'|is_active('^index-0.html', page)|safe }}><a class="nav-link active-bar" href="<?= base_url("akses"); ?>">Manajemen Hak Akses</a></li>
                 <?php } else { ?>
                     <li><a class="nav-link" href="<?= base_url("akses"); ?>">Manajemen Hak Akses</a></li>
                 <?php } ?>
