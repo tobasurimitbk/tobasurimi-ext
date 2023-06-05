d7a2bdeac87ec3eb2c65a753fcf477da893971a7
leonardo-wjy <leo.wijaya.0728@gmail.com>
2023-06-05 13:30:45 +0700

    no css

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -1,30 +1,4 @@
 <?= $this->extend('layouts/template'); ?>
 <?= $this->Section('content'); ?>
 
-<script>
-<?php
-if (session()->getFlashData("errors")) {
-?>
-    Swal.fire({
-        icon: 'error',
-        title: '<?= session()->getFlashData("errors"); ?>',
-        confirmButtonColor: '#4e73df',
-    })
-<?php
-}
-?>
-
-<?php
-if (session()->getFlashData("success")) {
-?>
-    Swal.fire({
-        icon: 'success',
-        title: '<?= session()->getFlashData("success"); ?>',
-        confirmButtonColor: '#4e73df',
-    })
-<?php
-}
-?>
-</script>
-
 <?= $this->endSection(); ?>
