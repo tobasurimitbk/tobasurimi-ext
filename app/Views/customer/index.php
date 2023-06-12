18a750dd8864dd17ead6b2629a627fc5bbe5e4da
leonardo-wjy <leo.wijaya.0728@gmail.com>
2023-06-12 17:11:35 +0700

    customer

--- app/Views/customer/index.php
+++ app/Views/customer/index.php
@@ -241,18 +241,18 @@
 <section class="section">
 <div class="section-header">
     <h1>Customer</h1>
+    <button class="btn btn-show-form btn-add btn-block float-right" data-btn="create-modal">
+        <i class="fa fa-plus fa-sm mr-2" aria-hidden="true"></i>Add New
+    </button>
 </div>
 <div class="row">
-    <div>
-        <div>
-            <button class="btn btn-show-form btn-add btn-block float-right" data-btn="create-modal" style="margin-top: -40px; width: 176px;">
-                <i class="fa fa-plus fa-sm mr-2" aria-hidden="true"></i>Add New
-            </button>
-        </div>
+    <div class="col-md-10">
     </div>
-    <div>
-        <input class="form-control search float-right" placeholder="Search" style="width: 30%" value="" />
+    <div class="col-md-2">
+        <input class="form-control search" placeholder="Search" value="" />
     </div>
+</div>
+<div class="row">
     <div class="table-responsive">
         <table class="table table-bordered nowrap table-hover-tobasurimi dataTable" id="dataTable" width="100%" cellspacing="0">
             <thead class="thead-dark">
