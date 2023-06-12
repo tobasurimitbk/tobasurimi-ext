a123c4344279508df95d1c6bdc32edbef75cd356
leonardo-wjy <leo.wijaya.0728@gmail.com>
2023-06-12 15:41:55 +0700

    update template link css js query version time

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -1,78 +1,219 @@
 <!-- Header -->
-<nav class="navbar navbar-expand-lg main-navbar navbar-border">
-    <a href="#" data-toggle="sidebar" class="nav-bar nav-link nav-link-lg"><i class="fas fa-bars"></i></a>
-    
-    <div class="dropdown dropdown-company">
-        <button class="btn btn-discard btn-dropdown-company dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-            <i class="fa fa-building fa-sm mr-2" aria-hidden="true"></i><?= session()->get("login")->this_company; ?>
-        </button>
-        <ul class="dropdown-menu list-dropdown-company" aria-labelledby="dropdownMenuButton1">
-            <?php foreach (session()->get("login")->company_role as $allCompany) { ?>
-                <li onclick="tes(<?= $allCompany->company_id; ?>, '<?= $allCompany->company_name; ?>')"><a class="<?= $allCompany->company_id == session()->get("login")->this_company_id ? "dropdown-item active" : "dropdown-item" ?>"><i class="fa fa-building fa-sm mr-2" aria-hidden="true"></i><?= $allCompany->company_name; ?></a></li>
-            <?php } ?>
+<nav class="navbar navbar-expand-lg main-navbar">
+    <form class="form-inline mr-auto">
+        <ul class="navbar-nav mr-3">
+            <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+            <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li>
         </ul>
-    </div>
-
-    <ul class="navbar-nav profile-button">
-        <li class="dropdown">
-            <a class="nav-link nav-link-lg nav-link-user dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <div class="mr-2 d-none d-lg-inline text-black-600 small"><?= session()->get("login")->name; ?></div>
-                <img alt="image" src="<?= base_url() ?>assets/img/avatar/avatar.png" class="rounded-circle mr-1">
+        <div class="search-element">
+            <input class="form-control" type="search" placeholder="Search" aria-label="Search" data-width="250">
+            <button class="btn" type="submit"><i class="fas fa-search"></i></button>
+            <div class="search-backdrop"></div>
+            <div class="search-result">
+                <div class="search-header">
+                    Histories
+                </div>
+                <div class="search-item">
+                    <a href="#">How to hack NASA using CSS</a>
+                    <a href="#" class="search-close"><i class="fas fa-times"></i></a>
+                </div>
+                <div class="search-item">
+                    <a href="#">Kodinger.com</a>
+                    <a href="#" class="search-close"><i class="fas fa-times"></i></a>
+                </div>
+                <div class="search-item">
+                    <a href="#">#Stisla</a>
+                    <a href="#" class="search-close"><i class="fas fa-times"></i></a>
+                </div>
+                <div class="search-header">
+                    Result
+                </div>
+                <div class="search-item">
+                    <a href="#">
+                        <img class="mr-3 rounded" width="30" src="<?= base_url() ?>/template/assets/img/products/product-3-50.png" alt="product">
+                        oPhone S9 Limited Edition
+                    </a>
+                </div>
+                <div class="search-item">
+                    <a href="#">
+                        <img class="mr-3 rounded" width="30" src="<?= base_url() ?>/template/assets/img/products/product-2-50.png" alt="product">
+                        Drone X2 New Gen-7
+                    </a>
+                </div>
+                <div class="search-item">
+                    <a href="#">
+                        <img class="mr-3 rounded" width="30" src="<?= base_url() ?>/template/assets/img/products/product-1-50.png" alt="product">
+                        Headphone Blitz
+                    </a>
+                </div>
+                <div class="search-header">
+                    Projects
+                </div>
+                <div class="search-item">
+                    <a href="#">
+                        <div class="search-icon bg-danger text-white mr-3">
+                            <i class="fas fa-code"></i>
+                        </div>
+                        Stisla Admin Template
+                    </a>
+                </div>
+                <div class="search-item">
+                    <a href="#">
+                        <div class="search-icon bg-primary text-white mr-3">
+                            <i class="fas fa-laptop"></i>
+                        </div>
+                        Create a new Homepage Design
+                    </a>
+                </div>
+            </div>
+        </div>
+    </form>
+    <ul class="navbar-nav navbar-right">
+        <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link nav-link-lg message-toggle beep"><i class="far fa-envelope"></i></a>
+            <div class="dropdown-menu dropdown-list dropdown-menu-right">
+                <div class="dropdown-header">Messages
+                    <div class="float-right">
+                        <a href="#">Mark All As Read</a>
+                    </div>
+                </div>
+                <div class="dropdown-list-content dropdown-list-message">
+                    <a href="#" class="dropdown-item dropdown-item-unread">
+                        <div class="dropdown-item-avatar">
+                            <img alt="image" src="<?= base_url() ?>/template/assets/img/avatar/avatar-1.png" class="rounded-circle">
+                            <div class="is-online"></div>
+                        </div>
+                        <div class="dropdown-item-desc">
+                            <b>Kusnaedi</b>
+                            <p>Hello, Bro!</p>
+                            <div class="time">10 Hours Ago</div>
+                        </div>
+                    </a>
+                    <a href="#" class="dropdown-item dropdown-item-unread">
+                        <div class="dropdown-item-avatar">
+                            <img alt="image" src="<?= base_url() ?>/template/assets/img/avatar/avatar-2.png" class="rounded-circle">
+                        </div>
+                        <div class="dropdown-item-desc">
+                            <b>Dedik Sugiharto</b>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+                            <div class="time">12 Hours Ago</div>
+                        </div>
+                    </a>
+                    <a href="#" class="dropdown-item dropdown-item-unread">
+                        <div class="dropdown-item-avatar">
+                            <img alt="image" src="<?= base_url() ?>/template/assets/img/avatar/avatar-3.png" class="rounded-circle">
+                            <div class="is-online"></div>
+                        </div>
+                        <div class="dropdown-item-desc">
+                            <b>Agung Ardiansyah</b>
+                            <p>Sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <div class="time">12 Hours Ago</div>
+                        </div>
+                    </a>
+                    <a href="#" class="dropdown-item">
+                        <div class="dropdown-item-avatar">
+                            <img alt="image" src="<?= base_url() ?>/template/assets/img/avatar/avatar-4.png" class="rounded-circle">
+                        </div>
+                        <div class="dropdown-item-desc">
+                            <b>Ardian Rahardiansyah</b>
+                            <p>Duis aute irure dolor in reprehenderit in voluptate velit ess</p>
+                            <div class="time">16 Hours Ago</div>
+                        </div>
+                    </a>
+                    <a href="#" class="dropdown-item">
+                        <div class="dropdown-item-avatar">
+                            <img alt="image" src="<?= base_url() ?>/template/assets/img/avatar/avatar-5.png" class="rounded-circle">
+                        </div>
+                        <div class="dropdown-item-desc">
+                            <b>Alfa Zulkarnain</b>
+                            <p>Exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+                            <div class="time">Yesterday</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="dropdown-footer text-center">
+                    <a href="#">View All <i class="fas fa-chevron-right"></i></a>
+                </div>
+            </div>
+        </li>
+        <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link notification-toggle nav-link-lg beep"><i class="far fa-bell"></i></a>
+            <div class="dropdown-menu dropdown-list dropdown-menu-right">
+                <div class="dropdown-header">Notifications
+                    <div class="float-right">
+                        <a href="#">Mark All As Read</a>
+                    </div>
+                </div>
+                <div class="dropdown-list-content dropdown-list-icons">
+                    <a href="#" class="dropdown-item dropdown-item-unread">
+                        <div class="dropdown-item-icon bg-primary text-white">
+                            <i class="fas fa-code"></i>
+                        </div>
+                        <div class="dropdown-item-desc">
+                            Template update is available now!
+                            <div class="time text-primary">2 Min Ago</div>
+                        </div>
+                    </a>
+                    <a href="#" class="dropdown-item">
+                        <div class="dropdown-item-icon bg-info text-white">
+                            <i class="far fa-user"></i>
+                        </div>
+                        <div class="dropdown-item-desc">
+                            <b>You</b> and <b>Dedik Sugiharto</b> are now friends
+                            <div class="time">10 Hours Ago</div>
+                        </div>
+                    </a>
+                    <a href="#" class="dropdown-item">
+                        <div class="dropdown-item-icon bg-success text-white">
+                            <i class="fas fa-check"></i>
+                        </div>
+                        <div class="dropdown-item-desc">
+                            <b>Kusnaedi</b> has moved task <b>Fix bug header</b> to <b>Done</b>
+                            <div class="time">12 Hours Ago</div>
+                        </div>
+                    </a>
+                    <a href="#" class="dropdown-item">
+                        <div class="dropdown-item-icon bg-danger text-white">
+                            <i class="fas fa-exclamation-triangle"></i>
+                        </div>
+                        <div class="dropdown-item-desc">
+                            Low disk space. Let's clean it!
+                            <div class="time">17 Hours Ago</div>
+                        </div>
+                    </a>
+                    <a href="#" class="dropdown-item">
+                        <div class="dropdown-item-icon bg-info text-white">
+                            <i class="fas fa-bell"></i>
+                        </div>
+                        <div class="dropdown-item-desc">
+                            Welcome to Stisla template!
+                            <div class="time">Yesterday</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="dropdown-footer text-center">
+                    <a href="#">View All <i class="fas fa-chevron-right"></i></a>
+                </div>
+            </div>
+        </li>
+        <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+                <img alt="image" src="<?= base_url() ?>/assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
+                <div class="d-sm-none d-lg-inline-block">Artinya apa Bang Messi?</div>
             </a>
-            <!-- Dropdown - User Information -->
-            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" onclick="showLogoutForm()" href="#">
-                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Logout
+            <div class="dropdown-menu dropdown-menu-right">
+                <div class="dropdown-title">Logged in 5 min ago</div>
+                <a href="features-profile.html" class="dropdown-item has-icon">
+                    <i class="far fa-user"></i> Profile
+                </a>
+                <a href="features-activities.html" class="dropdown-item has-icon">
+                    <i class="fas fa-bolt"></i> Activities
+                </a>
+                <a href="features-settings.html" class="dropdown-item has-icon">
+                    <i class="fas fa-cog"></i> Settings
+                </a>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item has-icon text-danger">
+                    <i class="fas fa-sign-out-alt"></i> Logout
                 </a>
             </div>
         </li>
     </ul>
 </nav>
-
-<!-- Logout Modal-->
-<div class="modal logout-modal" tabindex="-1">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title title-secondary">Ready to Leave?</h5>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-            <div class="modal-footer">
-                <button class="btn btn-discard" onclick="hideLogoutForm()">Cancel</button>
-                <a class="btn btn-logout-form" href="<?= base_url("logout"); ?>">Logout</a>
-            </div>
-        </div>
-    </div>
-</div>
-
-<script>
-    const showLogoutForm = function(e) {
-        $(".logout-modal").modal("show")
-    }
-
-    const hideLogoutForm = function(e) {
-        $(".logout-modal").modal("hide")
-    }
-
-    const tes = function(dropdownCompanyId, dropdownCompanyName) {
-        console.log(dropdownCompanyId)
-        $.ajax({
-            url: "<?= base_url("change-company"); ?>",
-            data: {
-                id: dropdownCompanyId,
-                name: dropdownCompanyName
-            },
-            method: "GET",
-            dataType: "json",
-            success: function(res) {
-                if (res.status) {
-                    window.location.href = '<?= base_url(); ?>'
-                }
-            }
-        })
-    }
-</script>
--- app/Views/layouts/template.php
+++ app/Views/layouts/template.php
@@ -2,113 +2,56 @@
 <html lang="en">
 
 <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <title><?= getenv("SITE_TITLE"); ?></title>
-    <link rel="shortcut icon" href="<?= base_url(); ?>assets/img/favicon.png" type="image/png" />
-
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css?v=<?= time(); ?>" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css?v=<?= time(); ?>" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
-    <link href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css?v=<?= time(); ?>" rel="stylesheet">
-    <link href="https://cdn.datatables.net/rowreorder/1.3.1/css/rowReorder.dataTables.min.css?v=<?= time(); ?>" rel="stylesheet">
-    <link href="https://cdn.datatables.net/responsive/2.4.0/css/responsive.dataTables.min.css?v=<?= time(); ?>" rel="stylesheet">
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css?v=<?= time(); ?>" />
-    
     <!-- Template CSS -->
     <link rel="stylesheet" href="<?= base_url() ?>/assets/css/style.css?v=<?= time(); ?>">
     <link rel="stylesheet" href="<?= base_url() ?>/assets/css/components.css?v=<?= time(); ?>">
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css?v=<?= time(); ?>" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.standalone.min.css?v=<?= time(); ?>" integrity="sha512-TQQ3J4WkE/rwojNFo6OJdyu6G8Xe9z8rMrlF9y7xpFbQfW5g8aSWcygCQ4vqRiJqFsDsE1T6MoAOMJkFXlrI9A==" crossorigin="anonymous" referrerpolicy="no-referrer">
-
-
-    <link href="<?= base_url(); ?>assets/_vendor/fontawesome-free/css/all.min.css?v=<?= time(); ?>" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css?v=<?= time(); ?>" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
-    <!-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"> -->
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap?v=<?= time(); ?>" rel="stylesheet">
-
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.43/css/bootstrap-datetimepicker.min.css?v=<?= time(); ?>"> 
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.43/css/bootstrap-datetimepicker-standalone.css?v=<?= time(); ?>"> 
- </head>
+</head>
 
 <body>
-    <!-- Bootstrap core JavaScript-->
-    <script src="<?= base_url(); ?>assets/_vendor/jquery/jquery.min.js?v=<?= time(); ?>"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js?v=<?= time(); ?>"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.43/js/bootstrap-datetimepicker.min.js?v=<?= time(); ?>"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js?v=<?= time(); ?>"></script>
-
-    <script src="<?= base_url(); ?>assets/js/datatables.min.js?v=<?= time(); ?>"></script>
-    <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js?v=<?= time(); ?>"></script>
-    <script src="https://cdn.datatables.net/rowreorder/1.3.1/js/dataTables.rowReorder.min.js?v=<?= time(); ?>"></script>
-    <script src="https://cdn.datatables.net/responsive/2.4.0/js/dataTables.responsive.min.js?v=<?= time(); ?>"></script>
-    <script src="https://cdn.datatables.net/responsive/2.4.0/js/responsive.bootstrap4.min.js?v=<?= time(); ?>"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11?v=<?= time(); ?>"></script>
-    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.js?v=<?= time(); ?>"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bs-custom-file-input/dist/bs-custom-file-input.js?v=<?= time(); ?>"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/5.0.7/jquery.inputmask.min.js?v=<?= time(); ?>" integrity="sha512-jTgBq4+dMYh73dquskmUFEgMY5mptcbqSw2rmhOZZSJjZbD2wMt0H5nhqWtleVkyBEjmzid5nyERPSNBafG4GQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js?v=<?= time(); ?>" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://unpkg.com/imask?v=<?= time(); ?>"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js?v=<?= time(); ?>" integrity="sha512-T/tUfKSV1bihCnd+MxKD0Hm1uBBroVYBOYSk1knyvQ9VyZJpc/ALb4P0r6ubwVPSGB2GvjeoMAJJImBG12TiaQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-    
-
     <div id="app">
         <!-- {% block content_2 %}{% endblock %} -->
-
         <div class="main-wrapper">
-            <!-- Sidebar -->
-            <?= $this->include('layouts/sidebar');  ?>
+            <div class="navbar-bg"></div>
 
             <!-- Header -->
             <?= $this->include('layouts/header'); ?>
 
+            <!-- Sidebar -->
+            <?= $this->include('layouts/sidebar');  ?>
+
+            <!-- Main Content -->
             <div class="main-content">
                 <?= $this->renderSection('content'); ?>
-
-                <!-- Footer -->
-                <?= $this->include('layouts/footer'); ?>
             </div>
+
+            <!-- Footer -->
+            <?= $this->include('layouts/footer'); ?>
         </div>
+        {% endif %}
     </div>
-    <script>
-        //Function Set Spinner Button
-        const setLoading = function() {
-            $(".delete-btn").attr("disabled", true)
-            $(".btn-submit-form").attr("disabled", true)
-        }
 
-        const stopLoading = function() {
-            $(".delete-btn").attr("disabled", false)
-            $(".btn-submit-form").attr("disabled", false)
-        }
-
-        const formatNumber = function(el) {
-            if (isNaN(el.value.replaceAll(",", ""))) {
-                el.value = ""
-            }
-            el.value = Number(el.value.replaceAll(".", "")).toLocaleString().replaceAll(",", ".")
-            return true
-        }
-    </script>
     <!-- General JS Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js?v=<?= time(); ?>"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js?v=<?= time(); ?>" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js?v=<?= time(); ?>" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js?v=<?= time(); ?>"></script>
-    <script src="<?= base_url() ?>/assets/js/stisla.js?v=<?= time(); ?>"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js?v=<?= time(); ?>"></script>
+    <script src="<?= base_url() ?>/template/assets/js/stisla.js?v=<?= time(); ?>"></script>
+
     <!-- Template JS File -->
-    <script src="<?= base_url() ?>assets/js/scripts.js?v=<?= time(); ?>"></script>
-    <script src="<?= base_url() ?>assets/js/custom.js?v=<?= time(); ?>"></script>
+    <script src="<?= base_url() ?>/assets/js/scripts.js?v=<?= time(); ?>"></script>
+    <script src="<?= base_url() ?>/assets/js/custom.js?v=<?= time(); ?>"></script>
+
+    <!-- Page Specific JS File -->
+    <script src="<?= base_url() ?>/assets/js/page/index-0.js?v=<?= time(); ?>"></script>
 
 </body>
 
