1ca8135457516706cf811eeef05b3cca55d3707b
kusyandi <kusyandi25@gmail.com>
2023-06-16 11:23:53 +0700

    style

--- app/Views/employee/index.php
+++ app/Views/employee/index.php
@@ -2,7 +2,7 @@
 <?= $this->Section('content'); ?>
 
 <div class="modal add-modal" tabindex="-1">
-    <div class="modal-dialog" style="max-width: 1200px !important;">
+    <div class="modal-dialog" style="min-width: 900px !important;">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title"><label class="title-name"></label> Employee</h5>
@@ -107,7 +107,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="row mb-3">
+                    <div class="row">
                         <div class="col-md-6">
                             <div class="form-floating mb-3" style="height: 50px;">
                                 <select class="form-select status" name="status" id="floatingSelect" aria-label="Floating label select example">
@@ -121,10 +121,12 @@
                 </form>
             </div>
             <div class="modal-footer justify-content-between">
-                <button type="button" class="btn btn-discard delete-btn">Hapus</button>
+                <div class="d-flex">
+                    <button type="button" class="btn btn-discard delete-btn">Hapus</button>
+                </div>
                 <label>&nbsp;</label>
                 <div class="d-flex">
-                    <button type="button" class="btn btn-hide-form btn-discard mr-3">Batal</button>
+                    <button type="button" class="btn btn-hide-form btn-discard mr-2">Batal</button>
                     <button type="submit" class="btn btn-submit-form">Simpan</button>
                 </div>
             </div>
@@ -144,7 +146,7 @@
     <div class="card-body">
         <div class="row justify-content-end mb-3">
             <div class="col-md-2">
-                <input class="form-control search" placeholder="Search" value="" />
+                <input class="form-control search form-out-search" placeholder="Search" value="" />
             </div>
         </div>
         <div class="row">
