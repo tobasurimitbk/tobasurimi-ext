6beaa89a10dd655aa7d0e503db10968129298323
kusyandi <kusyandi25@gmail.com>
2023-06-19 11:07:55 +0700

    style

--- app/Views/satuan/index.php
+++ app/Views/satuan/index.php
@@ -19,7 +19,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="row mb-3">
+                    <div class="row">
                         <div class="col-md-12">
                             <div class="form-floating mb-3" style="height: 50px;">
                                 <input type="text" class="form-control nama_satuan" id="nama_satuan" name="nama_satuan" placeholder="Nama Satuan">
@@ -30,10 +30,12 @@
                 </form>
             </div>
             <div class="modal-footer justify-content-between">
-                <button type="button" class="btn btn-discard delete-btn">Hapus</button>
+                <div class="d-flex">
+                    <button type="button" class="btn btn-discard delete-btn">Hapus</button>
+                </div>
                 <label>&nbsp;</label>
                 <div class="d-flex">
-                    <button type="button" class="btn btn-hide-form btn-discard mr-3">Batal</button>
+                    <button type="button" class="btn btn-hide-form btn-discard mr-2">Batal</button>
                     <button type="submit" class="btn btn-submit-form">Simpan</button>
                 </div>
             </div>
@@ -53,7 +55,7 @@
     <div class="card-body">
         <div class="row justify-content-end mb-3">
             <div class="col-md-2">
-                <input class="form-control search" placeholder="Search" value="" />
+                <input class="form-control search form-out-search" placeholder="Search" value="" />
             </div>
         </div>
         <div class="row">
