cedf40530d6dcf2040a903b10c8fade652054feb
frans <frans@lyrid.co.id>
2023-06-24 16:02:36 +0700

    update js ke file lokal

--- app/Views/layouts/template.php
+++ app/Views/layouts/template.php
@@ -54,13 +54,13 @@
     <script src="<?= base_url(); ?>assets/js/dataTables.responsive.min.js?v=<?= time(); ?>"></script>
     <script src="<?= base_url(); ?>assets/js/responsive.bootstrap4.min.js?v=<?= time(); ?>"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11?v=<?= time(); ?>"></script>
-    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.js?v=<?= time(); ?>"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bs-custom-file-input/dist/bs-custom-file-input.js?v=<?= time(); ?>"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/5.0.7/jquery.inputmask.min.js?v=<?= time(); ?>" integrity="sha512-jTgBq4+dMYh73dquskmUFEgMY5mptcbqSw2rmhOZZSJjZbD2wMt0H5nhqWtleVkyBEjmzid5nyERPSNBafG4GQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js?v=<?= time(); ?>" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="<?= base_url(); ?>assets/js/sweetalert2@11.js?v=<?= time(); ?>"></script>
+    <script src="<?= base_url(); ?>assets/js/jquery.validate.js?v=<?= time(); ?>"></script>
+    <script src="<?= base_url(); ?>assets/js/bs-custom-file-input.js?v=<?= time(); ?>"></script>
+    <script src="<?= base_url(); ?>assets/js/jquery.inputmask.min.js?v=<?= time(); ?>" integrity="sha512-jTgBq4+dMYh73dquskmUFEgMY5mptcbqSw2rmhOZZSJjZbD2wMt0H5nhqWtleVkyBEjmzid5nyERPSNBafG4GQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="<?= base_url(); ?>assets/js/jquery.mask.min.js?v=<?= time(); ?>" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="<?= base_url(); ?>assets/js/imask.js?v=<?= time(); ?>"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js?v=<?= time(); ?>" integrity="sha512-T/tUfKSV1bihCnd+MxKD0Hm1uBBroVYBOYSk1knyvQ9VyZJpc/ALb4P0r6ubwVPSGB2GvjeoMAJJImBG12TiaQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="<?= base_url(); ?>assets/js/bootstrap-datepicker.min.js?v=<?= time(); ?>" integrity="sha512-T/tUfKSV1bihCnd+MxKD0Hm1uBBroVYBOYSk1knyvQ9VyZJpc/ALb4P0r6ubwVPSGB2GvjeoMAJJImBG12TiaQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 
 
@@ -107,9 +107,9 @@
         }
     </script>
     <!-- General JS Scripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js?v=<?= time(); ?>" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js?v=<?= time(); ?>" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js?v=<?= time(); ?>"></script>
+    <script src="<?= base_url(); ?>assets/js/popper.min.js?v=<?= time(); ?>" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="<?= base_url(); ?>assets/js/bootstrap.min.js?v=<?= time(); ?>" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="<?= base_url(); ?>assets/js/jquery.nicescroll.min.js?v=<?= time(); ?>"></script>
     <script src="<?= base_url() ?>/assets/js/stisla.js?v=<?= time(); ?>"></script>
     <!-- Template JS File -->
     <script src="<?= base_url() ?>assets/js/scripts.js?v=<?= time(); ?>"></script>
