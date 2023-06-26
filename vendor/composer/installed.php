b96f08de9108f5017dd4f66272d104068c80ce11
kusyandi <kusyandi25@gmail.com>
2023-06-26 11:26:19 +0700

    style

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'codeigniter4/appstarter',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'c4ea6135f3bb1383723b90e40bd152e291b37418',
+        'reference' => '7b5446583bcd275baae96cd0a971457bb63cf23a',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'codeigniter4/appstarter' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'c4ea6135f3bb1383723b90e40bd152e291b37418',
+            'reference' => '7b5446583bcd275baae96cd0a971457bb63cf23a',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
