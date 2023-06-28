d6d81519251fe0e00a21b9fd23589bac36376151
Walidnurudin <walidnurudin47@gmail.com>
2023-06-28 13:55:41 +0700

    :zap: form pinjaman karyawan

--- app/Controllers/HR/PinjamanKaryawan.php
+++ app/Controllers/HR/PinjamanKaryawan.php
@@ -19,4 +19,130 @@ class PinjamanKaryawan extends BaseController
     {
         return view('hr/pinjamanKaryawan/index');
     }
+
+    public function createView()
+    {
+        return view('hr/pinjamanKaryawan/form');
+    }
+
+    public function create()
+    {
+        $rules = [
+            "nama_karyawan" => [
+                "rules" => "required"
+            ],
+            "tanggal_peminjaman" => [
+                "rules" => "required"
+            ],
+            "total_pinjaman" => [
+                "rules" => "required"
+            ],
+            "termin_pembayaran" => [
+                "rules" => "required"
+            ],
+        ];
+
+
+        if ($this->validate($rules)) {
+            $payload = json_encode([
+                "nama_karyawan" => $this->request->getPost("nama_karyawan"),
+                "tanggal_peminjaman" => $this->request->getPost("tanggal_peminjaman"),
+                "total_pinjaman" => $this->request->getPost("total_pinjaman"),
+                "termin_pembayaran" => $this->request->getPost("termin_pembayaran"),
+            ]);
+
+            $response = curl_request("POST", "/employeeLoan", $this->token, $payload);
+
+            if ($response["code"] === 201) {
+                $data = [
+                    "id" => json_decode($response["body"])->createdId,
+                    "status"            => true,
+                    "message"   => "Data Berhasil disimpan",
+                    "payload"   => $payload,
+                    'token' => csrf_hash(),
+                    'code' => $response["code"]
+                ];
+                echo json_encode($data);
+            } else {
+                $message = is_object(json_decode($response["body"])) ? json_decode($response["body"])->message : 'Data Gagal Disimpan';
+                $data = [
+                    "status"            => false,
+                    "message"    => $message,
+                    "payload"   => $payload,
+                    'token' => csrf_hash(),
+                    'code' => $response["code"]
+                ];
+                echo json_encode($data);
+            }
+        } else {
+            $data = [
+                "status"            => false,
+                "message"    => "Data Gagal Disimpan",
+                'token' => csrf_hash()
+            ];
+            echo json_encode($data);
+        }
+        return;
+    }
+
+    public function update()
+    {
+        $rules = [
+            "nama_karyawan" => [
+                "rules" => "required"
+            ],
+            "tanggal_peminjaman" => [
+                "rules" => "required"
+            ],
+            "total_pinjaman" => [
+                "rules" => "required"
+            ],
+            "termin_pembayaran" => [
+                "rules" => "required"
+            ],
+        ];
+
+        if ($this->validate($rules)) {
+            $id = $this->request->getPost("id");
+
+            $payload = json_encode([
+                "nama_karyawan" => $this->request->getPost("nama_karyawan"),
+                "tanggal_peminjaman" => $this->request->getPost("tanggal_peminjaman"),
+                "total_pinjaman" => $this->request->getPost("total_pinjaman"),
+                "termin_pembayaran" => $this->request->getPost("termin_pembayaran"),
+            ]);
+
+            $response = curl_request("PATCH", "/employeeLoan/$id", $this->token, $payload);
+
+            if ($response["code"] === 201) {
+                $data = [
+                    "id" => json_decode($response["body"])->createdId,
+                    "status"            => true,
+                    "message"   => "Data Berhasil diubah",
+                    "payload"   => $payload,
+                    'token' => csrf_hash(),
+                    'code' => $response["code"]
+                ];
+                echo json_encode($data);
+            } else {
+                $message = is_object(json_decode($response["body"])) ? json_decode($response["body"])->message : 'Data Gagal Diubah';
+                $data = [
+                    "status"            => false,
+                    "message"    => $message,
+                    "payload"   => $payload,
+                    'token' => csrf_hash(),
+                    'code' => $response["code"]
+                ];
+                echo json_encode($data);
+            }
+        } else {
+            $data = [
+                "status"            => false,
+                "message"    => "Data Gagal Diubah",
+                'token' => csrf_hash()
+            ];
+            echo json_encode($data);
+        }
+        return;
+    }
 }
