db1776e660c998e87df3d093c7a5ff4cabbf361c
Walidnurudin <walidnurudin47@gmail.com>
2023-07-06 13:36:48 +0700

    :zap: form surat jalan

--- app/Views/SalesLokal/Invoice/index.php
+++ app/Views/SalesLokal/Invoice/index.php
@@ -16,11 +16,11 @@
                     <table class="table nowrap table-hover-tobasurimi dataTable" id="dataTable" width="100%" cellspacing="0">
                         <thead class="thead-dark">
                             <tr>
-                                <th>#</th>
-                                <th>Nama Customer</th>
-                                <th>Destination</th>
-                                <th>Tipe</th>
-                                <th>Harga</th>
+                                <th>Kode Pelanggan</th>
+                                <th>Nama Pelanggan</th>
+                                <th>No So</th>
+                                <th>No Surat Jalan</th>
+                                <th>Shipping Date</th>
                             </tr>
                         </thead>
                         <tbody class="body-table" id="body-table" style="cursor: pointer;">
@@ -34,6 +34,84 @@
 </section>
 
 <script>
+    let sort = "";
+    let sortType = "asc";
+    let trigger = true;
 
+    let list_address = [];
+    let list_delete = [];
+    var row = 0;
+
+    const table = $('.dataTable').DataTable({
+        dom: "<'row'<'col-sm-12 col-md-6'><'col-sm-12 col-md-6'f>>t<'row align-items-start'<'col-md-4'l><'col-md-4 text-center'i><'col-md-4'p>>",
+        processing: true,
+        serverSide: true,
+        ordering: true,
+        order: [
+            [0, 'asc']
+        ],
+        fixedHeader: true,
+        lengthMenu: [
+            [25],
+            [25],
+        ],
+        pageLength: 25,
+        ajax: {
+            url: "<?= base_url("surat-jalan/all"); ?>",
+            dataSrc: "data",
+            data: function(data) {
+                data.search = $(".search").val();
+                data.sort = sort;
+                data.sortType = sortType;
+            }
+        },
+        // scrollX: true,
+        "initComplete": function(settings, json) {
+            $('.dataTables_length').empty();
+            $('.dataTables_length').html("<div><label class='text-center ml-2 mt-2'>Show <b class='entries-label'>25</b> Entries</label></div>");
+            $('.dataTable').wrap("<div style='overflow:auto; width:100%;position:relative;'></div>");
+        },
+        //responsive: true,
+        display: "stripe",
+        searching: false,
+        columns: [{
+            data: "kode_pelanggan",
+            className: "text-center"
+        }, {
+            data: "nama_pelanggan",
+            className: "text-center"
+        }, {
+            data: "multiple_no_so",
+            className: "text-center"
+        }, {
+            data: "no_surat_jalan",
+            className: "text-center"
+        }, {
+            data: "shipping_date",
+            className: "text-center"
+            // }, {
+            //     data: "id",
+            //     className: "text-center actions",
+            //     searchable: false,
+            //     sortable: false,
+            //     render: function(data, type, row) {
+            //         let id = row?.id;
+            //         return row.is_posted ? "-" : `<button type="button" onclick="handleDelete('${id}')" class="btn btn-discard delete-btn">Hapus</button>
+            //         `
+            //     }
+        }],
+        columnDefs: [{
+            defaultContent: "-",
+            targets: "_all"
+        }],
+        language: {
+            emptyTable: "Tidak Ada Data",
+            lengthMenu: "Show _MENU_ entries",
+            paginate: {
+                previous: '<i class="fa fa-angle-left"></i>',
+                next: '<i class="fa fa-angle-right"></i>'
+            }
+        }
+    });
 </script>
 <?= $this->endSection(); ?>
