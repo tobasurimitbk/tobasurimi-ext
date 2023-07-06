2c41eb3d898a7ccd0aff1b122377418b0f4153f8
kusyandi <kusyandi25@gmail.com>
2023-07-06 13:55:55 +0700

    style

--- app/Views/SalesLokal/OrderForm/index.php
+++ app/Views/SalesLokal/OrderForm/index.php
@@ -114,7 +114,7 @@
             sortable: false,
             render: function(data, type, row) {
                 let id = row?.id;
-                return row.is_posted ? "-" : `<button type="button" onclick="handleDelete('${id}')" class="btn btn-discard delete-btn">Hapus</button>
+                return row.is_posted ? "-" : `<button type="button" onclick="handleDelete('${id}')" class="btn btn-discard delete-btn btn-trash"><i class="fa fa-trash"></i></button>
                 `
             }
         }],
