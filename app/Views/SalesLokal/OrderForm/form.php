209d24c0de2f4c8c51c4936bedefdc62d9f070d0
kusyandi <kusyandi25@gmail.com>
2023-07-10 14:47:26 +0700

    style

--- app/Views/SalesLokal/OrderForm/form.php
+++ app/Views/SalesLokal/OrderForm/form.php
@@ -142,7 +142,7 @@
 
             <!-- list barang -->
             <div class="col-subtitle-modal">
-                <div class="row mt-2">
+                <div class="row mt-3">
                     <div class="col-md-6">
                         <label class="form-label font-weight-bold modal-sub-title">List Barang</label>
                     </div>
