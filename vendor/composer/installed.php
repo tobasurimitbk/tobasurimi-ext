a7003ff878fc084669d387cf07f25cef839743d0
Cahyo <cahyo@lyrid.co.id>
2023-07-12 12:14:34 +0700

    fix error

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,11 +3,7 @@
         'name' => 'codeigniter4/appstarter',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-<<<<<<< HEAD
-        'reference' => '488efed28da7db0e2a341bab1a139d75c8dd755e',
-=======
         'reference' => 'f78b492025273e673f99665fefcbd9cb0831e9a8',
->>>>>>> staging
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -17,11 +13,7 @@
         'codeigniter4/appstarter' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-<<<<<<< HEAD
-            'reference' => '488efed28da7db0e2a341bab1a139d75c8dd755e',
-=======
             'reference' => 'f78b492025273e673f99665fefcbd9cb0831e9a8',
->>>>>>> staging
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
