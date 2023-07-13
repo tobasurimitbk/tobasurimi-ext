df50866f5d9edb080a2c2fcaab6b2468884565e6
leonardo-wjy <leo.wijaya.0728@gmail.com>
2023-07-13 09:42:55 +0700

    barang updated

---
+++ app/Models/BarangModel.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class BarangModel extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'barangs';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = true;
+    protected $allowedFields    = [];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'createdAt';
+    protected $updatedField  = 'updatedAt';
+    protected $deletedField  = 'deletedAt';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+
+    public function getBarangList($condition, $addCondition, $limit = 10, $offset = 0)
+    {
+        $availableSort = [
+            'kode_barang'       => 'barangs.kode_barang',
+            'nama_barang'       => 'barangs.nama_barang',
+            'harga_barang'      => 'barangs.harga_barang',
+            'kode_satuan'       => 'satuans.kode_satuan',
+            'kategori'          => 'metadata.value',
+            'code_hs'           => 'hs_codes.code',
+            'sub_akun_ap'       => 'ap.nama_sub',
+            'sub_akun_ar'       => 'ar.nama_sub',
+            'stok'              => 'barangs.stok',
+            'status'            => 'barangs.status',
+            'createdAt'         => 'barangs.createdAt',
+            'updatedAt'         => 'barangs.updatedAt',
+        ];
+        $availableSortType = ['asc' => 'ASC', 'desc' => 'DESC'];
+
+        $sort = $availableSort[$addCondition['sort'] ?? 'createdAt'] ?? 'barangs.createdAt';
+        $sortType = $availableSortType[$addCondition['sortType'] ?? 'desc'] ?? 'DESC';
+
+        $selectQry = "barangs.*, 
+                      satuans.kode_satuan AS kode_satuan, 
+                      metadata.value AS kategori,
+                      hs_codes.code AS code_hs,
+                      ap.nama_sub AS sub_akun_ap,
+                      ar.nama_sub AS sub_akun_ar";
+        $barangDataQry = $this->asObject()
+            ->select($selectQry)
+            ->where($condition)
+            ->join('satuans', 'satuans.id = barangs.satuan_id')
+            ->join('metadata', 'metadata.id = barangs.kategori_id')
+            ->join('hs_codes', 'hs_codes.id = barangs.hs_id')
+            ->join('sub_akuns AS ap', 'ap.id = barangs.ap_id')
+            ->join('sub_akuns AS ar', 'ar.id = barangs.ar_id')
+            ->orderBy($sort, $sortType);
+
+        $totalData = $barangDataQry->countAllResults(false);
+
+        $barangDataQry->groupStart();
+
+        if ($addCondition['search']) {
+            $barangDataQry->like('nama_barang', $addCondition['search']);
+        }
+
+        if ($addCondition['kategori']) {
+            $barangDataQry->like('kategori', $addCondition['kategori']);
+        }
+
+        if ($addCondition['status']) {
+            $barangDataQry->like('barangs.status', $addCondition['status']);
+        }
+
+        $barangDataQry->groupEnd();
+        
+        $totalFilteredData = $barangDataQry->countAllResults(false);
+        $data = $barangDataQry->findAll($limit, $offset);
+
+        return [
+            'data'              => $data,
+            'totalData'         => $totalData,
+            'totalFilteredData' => $totalFilteredData,
+            'sort'  => $sort,
+            'sortType'  => $sortType
+        ];
+    }
+
+    public function getBarangByCompanyId($company_id)
+    {
+        $arrCondition = [
+            'deletedAt' => null,
+            'company_id' => $company_id
+        ];
+
+        $builder = $this->db->table('barangs');
+        $builder->where($arrCondition);
+        $query = $builder->get();
+        
+        return $query->getResult();
+    }
+
+    public function getBarangByKategori($kategori)
+    {
+        $arrCondition = [
+            'barangs.deletedAt' => null,
+            'metadata.deletedAt' => null,
+            'metadata.value' => $kategori
+        ];
+
+        $builder = $this->db->table('barangs')
+        ->join('metadata', 'metadata.id = barangs.kategori_id');
+        $builder->where($arrCondition);
+        $query = $builder->get();
+        
+        return $query->getResultArray();
+    }
+}
