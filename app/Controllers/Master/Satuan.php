b4968cfa886695346c76bed125ab27d4eb134482
frans <frans@lyrid.co.id>
2023-07-13 13:16:18 +0700

    convert satuan to php

--- app/Controllers/Master/Satuan.php
+++ app/Controllers/Master/Satuan.php
@@ -3,14 +3,17 @@
 namespace App\Controllers\Master;
 
 use App\Controllers\BaseController;
+use App\Models\SatuansModel;
 
 class Satuan extends BaseController
 {
     protected $token;
-    
+    protected $SatuansModel;
+
     public function __construct()
     {
         $this->token = session()->get("login")->token;
+        $this->SatuansModel = new SatuansModel();
     }
 
     public function satuan()
@@ -20,50 +23,59 @@ class Satuan extends BaseController
 
     public function allSatuan()
     {
-        $payload = [
-            "pageSize" => $this->request->getGet("length"),
-            "currentPage" => ($this->request->getGet("start") / $this->request->getGet("length")) + 1,
-            "search" => $this->request->getGet("search"),
-            "sort" => $this->request->getGet("sort"),
-            "sortType" => $this->request->getGet("sortType")
+
+        $draw = $this->request->getVar('draw');
+        $row = $this->request->getVar('start');
+        $rowperpage = $this->request->getVar('length');
+        $temp = $this->request->getVar('order');
+        $columnIndex = $temp[0]['column']; // Column index
+
+        $temp = $this->request->getVar('columns');
+        $columnName = $temp[$columnIndex]['data']; // Column index
+
+        $temp = $this->request->getVar('order');
+        $columnSortOrder = $temp[0]['dir']; // Column index
+
+        $search = $this->request->getVar('search');
+        //$searchValue = $temp['value']; // Column index
+
+        $values = [
+            "search"        => $search
         ];
 
-        $response = curl_request("GET", "/satuan", $this->token, $payload);
-        $dataSatuan = [];
-        $totalRecords = 0;
+        $totalRecords = $this->SatuansModel->total_list(array());
+        $totalRecordwithFilter = $this->SatuansModel->total_list($values);
 
-        if ($response["code"] === 200) {
-            $body = json_decode($response["body"])->data;
-            $totalRecords = json_decode($response["body"])->meta->totalData;
+        $res = $this->SatuansModel->search_list($values, $columnName . " " . $columnSortOrder, $row, $rowperpage);
 
-            $no = ($payload["pageSize"] * ($payload["currentPage"] - 1)) + 1;
+        $number = $row * $rowperpage;
 
-            foreach ($body as $data) {
-                array_push($dataSatuan, [
-                    "no" => $no++,
-                    "id" => $data->id,
-                    "kode_satuan" => $data->kode_satuan,
-                    "nama_satuan" => $data->nama_satuan
-                ]);
-            }
+        $data = [];
+
+        for ($i = 0; $i < count($res); $i++) {
+
+            $data[] = array(
+                "no" => ($row + $i + 1),
+                "id" => $res[$i]["id"],
+                "kode_satuan" => $res[$i]["kode_satuan"],
+                "nama_satuan" => $res[$i]["nama_satuan"],
+            );
         }
 
-        $data = [
-            "draw"            => intval($this->request->getGet("draw")),
-            "recordsTotal"    => $totalRecords,
-            "recordsFiltered" => $totalRecords,
-            "data" => $dataSatuan,
-            "response" => $response,
-            "payload" => $payload
-        ];
+        ## Response
+        $response = array(
+            "draw" => intval($draw),
+            "iTotalRecords" => $totalRecords,
+            "iTotalDisplayRecords" => $totalRecordwithFilter,
+            "aaData" => $data
+        );
 
-        echo json_encode($data);
-        return;
+        return $this->response->setJSON($response);
     }
 
     public function saveSatuan()
     {
-        try{
+        try {
             $rules = [
                 "kode_satuan" => [
                     "rules" => "required"
@@ -74,27 +86,25 @@ class Satuan extends BaseController
             ];
 
             if ($this->validate($rules)) {
-                $payload = json_encode([
+                $values = [
                     "kode_satuan" => $this->request->getPost("kode_satuan"),
                     "nama_satuan" => $this->request->getPost("nama_satuan")
-                ]);
+                ];
 
-                $response = curl_request("POST", "/satuan", $this->token, $payload);
-
-                if ($response["code"] === 200) {
+                if ($this->SatuansModel->insert($values)) {
                     $data = [
                         "status"            => true,
                         "message"   => "Data Berhasil disimpan",
-                        "payload"   => $payload,
+                        "payload"   => "",
                         'token' => csrf_hash()
                     ];
                     echo json_encode($data);
                 } else {
-                    $message = is_object(json_decode($response["body"])) ? json_decode($response["body"])->message : 'Data Gagal Disimpan';
+                    $message = 'Data Gagal Disimpan';
                     $data = [
                         "status"            => false,
                         "message"    => $message,
-                        "payload"   => $payload,
+                        "payload"   => "",
                         'token' => csrf_hash()
                     ];
                     echo json_encode($data);
@@ -107,9 +117,7 @@ class Satuan extends BaseController
                 ];
                 echo json_encode($data);
             }
-        }
-        catch(\Exception $e)
-        {
+        } catch (\Exception $e) {
             $data = [
                 "status"            => false,
                 "message"    => $e->getMessage() . ' in ' . $e->getFile() . ':' . $e->getLine(),
@@ -122,7 +130,7 @@ class Satuan extends BaseController
 
     public function updateSatuan()
     {
-        try{
+        try {
             $rules = [
                 "kode_satuan" => [
                     "rules" => "required"
@@ -135,23 +143,21 @@ class Satuan extends BaseController
             if ($this->validate($rules)) {
                 $id = $this->request->getPost("id");
 
-                $payload = json_encode([
+                $values = [
                     "kode_satuan" => $this->request->getPost("kode_satuan"),
                     "nama_satuan" => $this->request->getPost("nama_satuan")
-                ]);
+                ];
 
-                $response = curl_request("PATCH", "/satuan/$id", $this->token, $payload);
-
-                if ($response["code"] === 200) {
+                if ($this->SatuansModel->update($id, $values)) {
                     $data = [
                         "status"            => true,
                         "message"   => "Data Berhasil disimpan",
-                        "payload"   => $payload,
+                        "payload"   => "",
                         'token' => csrf_hash()
                     ];
                     echo json_encode($data);
                 } else {
-                    $message = is_object(json_decode($response["body"])) ? json_decode($response["body"])->message : 'Data Gagal Diubah';
+                    $message = 'Data Gagal Diubah';
                     $data = [
                         "status"            => false,
                         "message"    => $message,
@@ -168,9 +174,7 @@ class Satuan extends BaseController
                 ];
                 echo json_encode($data);
             }
-        }
-        catch(\Exception $e)
-        {
+        } catch (\Exception $e) {
             $data = [
                 "status"            => false,
                 "message"    => $e->getMessage() . ' in ' . $e->getFile() . ':' . $e->getLine(),
@@ -184,15 +188,15 @@ class Satuan extends BaseController
     public function getByIdSatuan($id = null)
     {
         if (!empty($id)) {
-            $response = curl_request("GET", "/satuan/$id", $this->token);
-            if ($response["code"] === 200) {
+            $res = $this->SatuansModel->get_by_id($id);
+            if (count($res)) {
                 $data = [
                     "status"  => true,
-                    "data"  => json_decode($response["body"])->data,
+                    "data"  => (object) $res[0],
                 ];
                 echo json_encode($data);
             } else {
-                $message = is_object(json_decode($response["body"])) ? json_decode($response["body"])->message : 'Data Gagal Ditemukan';
+                $message = 'Data Gagal Ditemukan';
                 $data = [
                     "status" => false,
                     "message"  => $message
@@ -211,12 +215,14 @@ class Satuan extends BaseController
 
     public function deleteSatuan()
     {
-        try{
+        try {
             $id = $this->request->getPost("id");
 
             if (!empty($id)) {
-                $response = curl_request("DELETE", "/satuan/$id", $this->token);
-                if ($response["code"] === 200) {
+                $values = [
+                    "deletedAt" => date("Y-m-d H:i:s")
+                ];
+                if ($this->SatuansModel->update($id, $values)) {
                     $data = [
                         "status"            => true,
                         "message"   => "Data Berhasil dihapus",
@@ -224,7 +230,7 @@ class Satuan extends BaseController
                     ];
                     echo json_encode($data);
                 } else {
-                    $message = is_object(json_decode($response["body"])) ? json_decode($response["body"])->message : 'Data Gagal Dihapus';
+                    $message = 'Data Gagal Dihapus';
                     $data = [
                         "status"            => false,
                         "message"    => $message,
@@ -240,9 +246,7 @@ class Satuan extends BaseController
                 ];
                 echo json_encode($data);
             }
-        }
-        catch(\Exception $e)
-        {
+        } catch (\Exception $e) {
             $data = [
                 "status"            => false,
                 "message"    => $e->getMessage() . ' in ' . $e->getFile() . ':' . $e->getLine(),
@@ -255,13 +259,7 @@ class Satuan extends BaseController
 
     public function dropdownSatuan()
     {
-        $responseSatuan = curl_request("GET", "/satuan/all", $this->token);
-
-        $dataSatuan = [];
-        if ($responseSatuan["code"] === 200) {
-            $dataSatuan = json_decode($responseSatuan["body"])->data;
-        }
-
+        $dataSatuan = $this->SatuansModel->search_list(array(), 'nama_satuan');
         $data = [
             "data" => $dataSatuan
         ];
