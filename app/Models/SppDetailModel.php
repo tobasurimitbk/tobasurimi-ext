62a81b3a08d93af21223bc93613b36137fb35f1d
HafidhRizky <hafidhrzk@gmail.com>
2023-07-18 13:10:06 +0700

    delete SPP

--- app/Models/SppDetailModel.php
+++ app/Models/SppDetailModel.php
@@ -12,7 +12,7 @@ class SppDetailModel extends Model
     protected $useAutoIncrement = true;
     protected $insertID         = 0;
     protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
+    protected $useSoftDeletes   = true;
     protected $protectFields    = true;
     protected $allowedFields    = [
         "purchase_request_id",
