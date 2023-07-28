9d23f653c7224e5c2d4f715bd9afc67545a23f7f
leonardo-wjy <leo.wijaya.0728@gmail.com>
2023-07-28 15:56:45 +0700

    order form internasional

--- app/Controllers/SalesInternasional/OrderForm.php
+++ app/Controllers/SalesInternasional/OrderForm.php
@@ -4,18 +4,27 @@ namespace App\Controllers\SalesInternasional;
 
 use App\Controllers\BaseController;
 use Config\Services;
+use App\Models\MetadataModel;
+use App\Models\SalesOrderModel;
+use App\Models\SalesOrderDetailModel;
 
 class OrderForm extends BaseController
 {
     protected $token;
     protected $this_company_id;
     protected $encrypter;
+    protected $MetadataModel;
+    protected $SalesOrderModel;
+    protected $SalesOrderDetailModel;
 
     public function __construct()
     {
         $this->token = session()->get("login")->token;
         $this->this_company_id = session()->get("login")->this_company_id;
         $this->encrypter = Services::encrypter();
+        $this->MetadataModel = new MetadataModel();
+        $this->SalesOrderModel = new SalesOrderModel();
+        $this->SalesOrderDetailModel = new SalesOrderDetailModel();
     }
 
     public function index()
@@ -25,11 +34,69 @@ class OrderForm extends BaseController
 
     public function createView()
     {
-        return view('SalesInternasional/OrderForm/form');
+        //Get Valuta Asing By Metadata
+        $dataValuta = $this->MetadataModel->get_by_name('Valuta Asing');
+
+        $data = [
+            "dataValuta" => $dataValuta
+        ];
+        return view('SalesInternasional/OrderForm/form', $data);
     }
 
     public function all()
     {
+        $pageSize = $this->request->getGet("length");
+        $currentPage = ($this->request->getGet("start") / $this->request->getGet("length")) + 1;
+        $offset = $currentPage - 1;
+
+        $payload = [
+            "pageSize" => $pageSize,
+            "currentPage" => $currentPage,
+            "search" => $this->request->getGet("search"),
+            "sort" => $this->request->getGet("sort"),
+            "sortType" => $this->request->getGet("sortType"),
+        ];
+
+
+        $condition = [];
+
+        $addCondition = [
+            "search"        => $this->request->getGet("search"),
+            "sort"          => $this->request->getGet("sort"),
+            "sortType"      => $this->request->getGet("sortType"),
+            "dateStart"     => $this->request->getGet("dateStart") ? date("Y-m-d", strtotime(str_replace("/", "-", $this->request->getGet("dateStart")))) : "",
+            "dateEnd"       => $this->request->getGet("dateEnd") ? date("Y-m-d", strtotime(str_replace("/", "-", $this->request->getGet("dateEnd")))) : "",
+        ];
+
+        $dataOrderForm = $this->SalesOrderModel
+            ->getAllSalesOrderImport($condition, $addCondition, $pageSize, $offset);
+
+        $no = ($payload["pageSize"] * ($payload["currentPage"] - 1)) + 1;
+
+        $dataSalesOrder = [];
+        foreach ($dataOrderForm['data'] as $data) {
+            array_push($dataSalesOrder, [
+                "no"            => $no++,
+                "id"            => $data->id,
+                "no_sales_order"      => $data->no_sales_order,
+                "destination"        => $data->destination,
+                "qty_barang" => $data->qty_barang,
+                "total_harga"         => number_format($data->total_harga),
+                "keterangan"  => $data->keterangan
+            ]);
+        }
+
+
+        $data = [
+            "draw"            => intval($this->request->getGet("draw")),
+            "recordsTotal"    => $dataOrderForm['totalData'],
+            "recordsFiltered" => $dataOrderForm['totalFilteredData'],
+            "data"              => $dataSalesOrder,
+            "payload"           => $payload
+        ];
+
+        echo json_encode($data);
+        return;
     }
 
     public function save()
--- app/Views/SalesInternasional/OrderForm/index.php
+++ app/Views/SalesInternasional/OrderForm/index.php
@@ -10,27 +10,32 @@
         </a>
     </div>
     <div class="card">
-    </div>
-    <div class="row">
-        <div class="table-responsive">
-            <table class="table nowrap table-hover-tobasurimi dataTable" id="dataTable" width="100%" cellspacing="0">
-                <thead class="thead-dark">
-                    <tr>
-                        <th>No.</th>
-                        <th>No. Penawaran</th>
-                        <th>Nama Customer</th>
-                        <th>Valas</th>
-                        <th>Expired Date</th>
-                        <th>Tanggal Pembuatan</th>
-                    </tr>
-                </thead>
-                <tbody class="body-table" id="body-table" style="cursor: pointer;">
+        <div class="card-body">
+            <div class="row justify-content-end row-col-spp">
+                <div class="col-md-4 mb-3">
+                    <input class="form-control search form-out-search" placeholder="Cari No. Penawaran" value="" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="table-responsive">
+                    <table class="table nowrap table-hover-tobasurimi dataTable" id="dataTable" width="100%" cellspacing="0">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th>No.</th>
+                                <th>No. Penawaran</th>
+                                <th>Nama Customer</th>
+                                <th>Valas</th>
+                                <th>Expired Date</th>
+                                <th>Tanggal Pembuatan</th>
+                            </tr>
+                        </thead>
+                        <tbody class="body-table" id="body-table" style="cursor: pointer;">
 
-                </tbody>
-            </table>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-    </div>
-    </div>
     </div>
 </section>
 
