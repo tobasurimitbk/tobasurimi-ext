0c0e4c2b16b43e83bce885208aa4b5e3d1f71141
adittt <adit_83@ymail.com>
2023-08-08 15:27:41 +0700

    production result

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -368,7 +368,7 @@ $routes->post('/order-form-internasional/update-status', 'SalesInternasional\Ord
 // Production Result
 $routes->get('/production-result', 'Production\ProductionResult::index', ['filter' => 'Auth']);
 $routes->get('/production-result/(:num)', 'Production\ProductionResult::getById/$1', ['filter' => 'Auth']);
-$routes->get('/production-result/all', 'Production\ProductionResult::all', ['filter' => 'Auth']);
+$routes->get('/production-result/all', 'Production\ProductionResult::getAll', ['filter' => 'Auth']);
 $routes->get('/production-result/create', 'Production\ProductionResult::createProductionResult', ['filter' => 'Auth']);
 $routes->post('/production-result/create', 'Production\ProductionResult::saveProductionResult', ['filter' => 'Auth']);
 
---
+++ app/Models/ProductionResultModel.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ProductionResultModel extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'production_results';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'work_order_id',
+        'warehouse_id',
+        'pr_no',
+        'receive_date'
+    ];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'createdAt';
+    protected $updatedField  = 'updatedAt';
+    protected $deletedField  = 'deletedAt';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+
+    public function getProductResultList($condition, $addCondition, $limit = 10, $offset = 0)
+    {
+        $availableSort = [
+            'productionCode'=> 'production_results.pr_no',
+            'workOrderCode' => 'suppliers.kode',
+            'barangCode'    => 'barangs.kode_barang',
+            'barangName'    => 'barangs.nama_barang',
+            'warehouseName' => 'warehouses.warehouse_name',
+            'createdAt'     => 'production_results.createdAt'
+        ];
+        $availableSortType = ['asc' => 'ASC', 'desc' => 'DESC'];
+
+        $sort = $availableSort[$addCondition['sort'] ?? 'createdAt'] ?? 'production_results.createdAt';
+        $sortType = $availableSortType[$addCondition['sortType'] ?? 'desc'] ?? 'DESC';
+
+        $selectQry = "production_results.*, 
+                      DATE_FORMAT(production_results.receive_date, '%d/%m/%Y') AS receive_date,
+                      warehouses.warehouse_name AS warehouseName, 
+                      work_orders.wo_no AS wo_no,
+                      barangs.kode_barang AS barangCode,
+                      barangs.nama_barang AS barangName
+                      ";
+        $productionResDataQry = $this->asObject()
+            ->select($selectQry)
+            ->where($condition)
+            ->join('warehouses', 'warehouses.id = production_results.warehouse_id')
+            ->join('work_orders', 'work_orders.id = production_results.work_order_id')
+            ->join('barangs', 'barangs.id = work_orders.barang_id')
+            ->orderBy($sort, $sortType);
+
+        $totalData = $productionResDataQry->countAllResults(false);
+
+        if ($addCondition['search']) {
+            $productionResDataQry->groupStart()
+                ->like('production_results.pr_no', $addCondition['search'], 'after')
+                ->orLike('work_orders.wo_no', $addCondition['search'], 'after')
+                ->orLike('barangs.nama_barang', $addCondition['search'], 'after')
+            ->groupEnd();
+        }
+        
+        $totalFilteredData = $productionResDataQry->countAllResults(false);
+        $data = $productionResDataQry->findAll($limit, $offset);
+
+        return [
+            'data'              => $data,
+            'totalData'         => $totalData,
+            'totalFilteredData' => $totalFilteredData
+        ];
+    }
+}
