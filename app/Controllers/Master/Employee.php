bc8998a5bc5bda6c1903b68c20a74164fd4d1543
HafidhRizky <hafidhrzk@gmail.com>
2023-08-15 16:29:03 +0700

    S

--- app/Controllers/Master/Employee.php
+++ app/Controllers/Master/Employee.php
@@ -451,10 +451,23 @@ class Employee extends BaseController
                     }
                 }
 
-                dd($values);
-
                 if (isset($values)) {
                     if ($this->EmployeesModel->update($id, $values)) {
+                        foreach ($values["komponen_gaji"] as $value) {
+                            if (!empty($value->isDeleted)) {
+                                $this->GajiConjunctionModel->where('id', $value->id)->delete();
+                            }
+
+                            $dataDetail = [
+                                "id" => $value->id ?? null,
+                                "employee_id" => $this->request->getPost("id"),
+                                "tunjangan_id" => $value->tunjangan_id,
+                                "nominal" => $value->nominal,
+                            ];
+
+                            $this->GajiConjunctionModel->upsert($dataDetail);
+                        }
+
                         $data = [
                             "status"    => true,
                             "message"   => "Data Berhasil diubah",
@@ -550,6 +563,7 @@ class Employee extends BaseController
                 ];
                 $UserModel->where('employee_id', $id)->delete();
                 if ($this->EmployeesModel->update($id, $values)) {
+                    $this->GajiConjunctionModel->where('employee_id', $id)->delete();
                     $data = [
                         "status"            => true,
                         "message"   => "Data Berhasil dihapus",
