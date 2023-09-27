72021cdec92a39f2e33ff98b34d1dac06a9f6118
Cahyo <cahyo@lyrid.co.id>
2023-09-27 11:48:56 +0700

    hide terms

--- app/Views/SalesLokal/OrderForm/form.php
+++ app/Views/SalesLokal/OrderForm/form.php
@@ -66,14 +66,14 @@
                     </div>
                     <div class="col-md-4">
                         <div class="form-floating mb-3" style="height: 50px;">
-                            <input autocomplete="one-time-code" type="text" class="form-control" id="termin" name="termin" disabled value="<?= $data->termin ?? '' ?>">
-                            <label for="floatingInput">Termin</label>
+                            <input autocomplete="one-time-code" type="text" class="form-control" id="tagihan_ke" name="tagihan_ke" disabled value="<?= $data->address ?? '' ?>">
+                            <label for="floatingInput">Tagihan Ke</label>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="form-floating mb-3" style="height: 50px;">
-                            <input autocomplete="one-time-code" type="text" class="form-control" id="tagihan_ke" name="tagihan_ke" disabled value="<?= $data->address ?? '' ?>">
-                            <label for="floatingInput">Tagihan Ke</label>
+                            <input autocomplete="one-time-code" type="text" class="form-control" id="termin" name="termin" disabled value="<?= $data->termin ?? '' ?>">
+                            <label for="floatingInput">Termin</label>
                         </div>
                     </div>
                 </div>
@@ -91,12 +91,12 @@
                             <label for="floatingInput">Biaya Kirim</label>
                         </div>
                     </div>
-                    <div class="col-md-4">
+                    <!-- <div class="col-md-4">
                         <div class="form-floating mb-3" style="height: 50px;">
                             <input autocomplete="one-time-code" type="text" class="form-control payment_terms" id="payment_terms" name="payment_terms" <?= !empty($data) ? ($data->payment_terms === true ? 'disabled=true' : '') : ''; ?> placeholder="Terms" value="<?= !empty($data) ? $data->payment_terms : ""; ?>">
                             <label for="floatingInput">Terms</label>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
                 <div class="row">
                     <div class="col-md-4">
