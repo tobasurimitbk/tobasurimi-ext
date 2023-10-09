366e5b6f341e8e52a8d2372cef492495468bc5d2
Ardhikarestu <ardhikayoviyanto@gmail.com>
2023-10-09 16:21:26 +0700

    fixbug(ajaxfilter)

--- app/Models/EmployeesModel.php
+++ app/Models/EmployeesModel.php
@@ -109,7 +109,7 @@ class EmployeesModel extends Model
         if (isset($values["name"]))
             $requete .= ($values["name"] == "") ? "" : ("AND UPPER(employees.name) like '%" . strtoupper($values["name"]) . "%' ");
         if (isset($values["search"]))
-            $requete .= ($values["search"] == "") ? "" : ("AND (UPPER(employees.name) like '%" . strtoupper($values["search"]) . "%' OR UPPER(employees.nik) like '%" . strtoupper($values["search"]) . "%' OR UPPER(employees.nip) like '%" . strtoupper($values["search"]) . "%' OR UPPER(employees.jabatan) like '%" . strtoupper($values["search"]) . "%') ");
+            $requete .= ($values["search"] == "") ? "" : ("AND (UPPER(employees.name) like '%" . strtoupper($values["search"]) . "%' OR UPPER(employees.nik) like '%" . strtoupper($values["search"]) . "%' OR UPPER(employees.nip) like '%" . strtoupper($values["search"]) . "%' ");
 
         $result = $this->db->query($requete)->getResultArray();
         return ($result[0]["total"]) ? $result[0]["total"] : 0;
