f1fda8dfd8366fe8623595cc3d7d8b6c99b947d1
leonardo-wjy <leo.wijaya.0728@gmail.com>
2023-10-17 14:50:29 +0700

    supplier harga updated

--- app/Views/Supplier/supplierBahanBaku/harga.php
+++ app/Views/Supplier/supplierBahanBaku/harga.php
@@ -110,7 +110,7 @@
             </div>
             <div class="row">
                 <div class="table-responsive">
-                    <table class="table table-bordered nowrap table-hover-tobasurimi dataTable table-tambah-spp" width="100%" cellspacing="0">
+                <table class="table table-bordered nowrap table-hover-tobasurimi dataTable" id="dataTable" width="100%" cellspacing="0">
                         <thead class="thead-dark">
                             <tr>
                                 <th width="10">No</th>
