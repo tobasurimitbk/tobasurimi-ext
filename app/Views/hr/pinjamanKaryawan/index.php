6fdcea7a6a155cc924db0f30edda2cb5ac92aeda
Ardhikarestu <ardhikayoviyanto@gmail.com>
2023-10-18 16:47:15 +0700

    featbug(typo)

--- app/Views/hr/pinjamanKaryawan/index.php
+++ app/Views/hr/pinjamanKaryawan/index.php
@@ -424,15 +424,21 @@
                     let startDate = row?.mulaiAbsen;
                     let finishDate = row?.selesaiAbsen;
                     let tipeGol = row?.tipeGol;
+                    let status_pinjaman = row?.statusPinjaman;
                     let id = row?.id;
 
-                    return `
+                    if (status_pinjaman == 1) {
+                        return '-';
+                    } else {
+                        return `
             <div class="mt-0">
                 <button onclick="generateSingle(${employeeID}, '${employeeName}', '${yearMonth}', '${startDate}', '${finishDate}', '${tipeGol}', ${id})" class="btn btn-success posting-spp">
                     <i class="fa-solid fa-sm fa-repeat"></i>
                 </button>
             </div>
         `
+                    }
+
                 }
             }
 
