2cc64eef2027b5f77a244a548e31bf5dc5b7ec0d
Ardhikarestu <ardhikayoviyanto@gmail.com>
2024-02-06 13:35:40 +0700

    TYPO SPP

--- app/Controllers/Purchase/SPP.php
+++ app/Controllers/Purchase/SPP.php
@@ -211,6 +211,7 @@ class SPP extends BaseController
             "request_date" => $this->request->getVar("request_date") ? date("Y/m/d", strtotime(str_replace("/", "-", $this->request->getVar("request_date")))) : "",
             'spp_no' => $this->request->getVar('spp_no'),
             'divisi_id' => $this->request->getVar('divisi_id'),
+            'spp_type' => $this->request->getVar('spp_type'),
             'note' => $this->request->getVar('note'),
         ]);
 
