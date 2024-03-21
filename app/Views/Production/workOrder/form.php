5de6c4bc3c33a6c83055f5242f9e8ee469e0d15f
MuhammadAgungPutraNugroho <muhammadagung0708@example.com>
2024-03-21 09:52:22 +0700

    fix datepicker

--- app/Views/Production/workOrder/form.php
+++ app/Views/Production/workOrder/form.php
@@ -22,7 +22,7 @@
                 <div class="row">
                     <div class="col-md-4">
                         <div class="form-floating mb-3" style="height: 50px;">
-                            <input autocomplete="one-time-code" type="text" value="<?= !empty($dataWorkOrders) ? ($dataWorkOrders->request_date ? date("d/m/Y", strtotime($dataWorkOrders->request_date)) : "") : date("d/m/Y"); ?>" class="form-control date_production" name="date_production" id="date_production" placeholder="Tanggal Pembuatan Dokumen">
+                            <input autocomplete="one-time-code" value="<?= !empty($dataWorkOrders) ? ($dataWorkOrders->request_date ? date("d/m/Y", strtotime($dataWorkOrders->request_date)) : "") : ""; ?>" type="text" class="form-control date_production" name="date_production" id="date_production" placeholder="Tanggal Pembuatan Dokumen">
                             <label for="floatingInput">Tanggal Pembuatan Dokumen</label>
                         </div>
                     </div>
@@ -286,10 +286,13 @@
         var yyyy = today.getFullYear();
 
         today = dd + '/' + mm + '/' + yyyy;
-        $('#date_production').val(today);
-
+        <?php if (empty($dataWorkOrders)) : ?>
+            $('#date_production').val(today);
+        <?php endif; ?>
         // Mengaktifkan datepicker
-        $('#date_production').datepicker();
+        $('#date_production').datepicker({
+            enableOnReadonly: false
+        });
 
         // Membuat input readonly
         $('#date_production').prop('readonly', true);
