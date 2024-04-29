074efce1a0c8c90f79779ca9482f22fb589bd10c
Ardhikarestu <ardhikayoviyanto@gmail.com>
2024-04-29 14:37:37 +0700

    FEATBUG (ATTANDENCE MODULE)

--- app/Models/AttendancesModel.php
+++ app/Models/AttendancesModel.php
@@ -31,9 +31,9 @@ class AttendancesModel extends Model
     // Dates
     protected $useTimestamps = false;
     protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
+    protected $createdField  = 'createdAt';
+    protected $updatedField  = 'updatedAt';
+    protected $deletedField  = 'deletedAt';
 
     // Validation
     protected $validationRules      = [];
