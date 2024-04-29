9feb27ffb0b956b0ee6d20efd3bd23b7a15465eb
MuhammadAgungPutraNugroho <muhammadagung0708@example.com>
2024-04-29 14:38:00 +0700

    fix edit dan hapus detail

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -233,6 +233,7 @@ $routes->post('/spp/update', 'Purchase\SPP::updateSPP', ['filter' => 'Auth']);
 $routes->post('/spp/update-status', 'Purchase\SPP::updateStatusSPP', ['filter' => 'Auth']);
 $routes->post('/spp/approve', 'Purchase\SPP::approveSPP', ['filter' => 'Auth']);
 $routes->post('/spp/delete', 'Purchase\SPP::deleteSPP', ['filter' => 'Auth']);
+$routes->post('/spp/delete-detail', 'Purchase\SPP::deleteSPPDetail', ['filter' => 'Auth']);
 $routes->get('/spp/print-table', 'Purchase\SPP::printTable', ['filter' => 'Auth']);
 $routes->get('/spp/print/(:segment)', 'Purchase\SPP::print/$1', ['filter' => 'Auth']);
 
