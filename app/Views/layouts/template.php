f214c10774c8b04660f2496cf9e627429305964e
Ardhikarestu <ardhikayoviyanto@gmail.com>
2024-04-30 14:22:19 +0700

    add polifily

--- app/Views/layouts/template.php
+++ app/Views/layouts/template.php
@@ -42,7 +42,7 @@
 </head>
 
 <body>
-    <!-- Bootstrap core JavaScript-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/js-polyfills/0.1.43/polyfill.min.js" integrity="sha512-lvWiOP+aMKHllm4THsjzNleVuGOh0WGniJ3lgu/nvCbex1LlaQSxySUjAu/LTJw9FhnSL/PVYoQcckg1Q03+fQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> <!-- Bootstrap core JavaScript-->
     <script src="<?= base_url(); ?>assets/_vendor/jquery/jquery.min.js?v=<?= time(); ?>"></script>
     <script src="<?= base_url() ?>assets/js/moment.min.js?v=<?= time(); ?>"></script>
     <script type="text/javascript" src="<?= base_url() ?>assets/js/bootstrap-datetimepicker.min.js?v=<?= time(); ?>"></script>
