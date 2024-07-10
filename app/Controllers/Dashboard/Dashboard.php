7542607dcd792b80dd8756065a85fc04065fd0a0
KBumstead <115068127+KBumstead@users.noreply.github.com>
2024-07-10 15:08:21 +0700

    dashboard

--- app/Controllers/Dashboard/Dashboard.php
+++ app/Controllers/Dashboard/Dashboard.php
@@ -8,26 +8,47 @@ use App\Controllers\BaseController;
 use App\Models\RMImportPOModel;
 use App\Models\RMPurchaseOrderModel;
 use App\Models\AMPurchaseOrderModel;
+use App\Models\CustomerModel;
 use App\Models\SalesOrderModel;
 use App\Models\SalesOrderExportModel;
 use App\Models\SalesOrderLainModel;
+use App\Models\UserModel;
+use App\Models\WarehousesModel;
+use App\Models\WorkOrdersModel;
 use PDO;
 
 class Dashboard extends BaseController
 {
+    protected $this_company_id;
+
+    public function __construct()
+    {
+        $this->this_company_id = session()->get("login")->this_company_id;
+    }
     public function dashboard()
     {
+
         $rmPurchaseOrderModel = new RMPurchaseOrderModel();
         $amPurchaseOrderModel = new AMPurchaseOrderModel();
         $rmImportPOModel = new RMImportPOModel();
         $salesOrderModel = new SalesOrderModel();
         $salesOrderExportModel = new SalesOrderExportModel();
         $salesOrderLainModel = new SalesOrderLainModel();
+        $warehouseModel = new WarehousesModel();
+        $workOrdersModel = new WorkOrdersModel();
+        $userModel = new UserModel();
+        $customerModel = new CustomerModel();
 
         $jumlah_pembayaran_belum_posting = 0;
         $jumlah_sales_bulan_ini = 0;
+        $jumlah_warehouse_bulan_ini = 0;
 
+        $condition = [
+            'company_id' => $this->this_company_id,
+            'deletedAt'  => null
+        ];
         $selectQry = "sum(is_posted = 0) as total_belum_post";
+
 
         $jumlah_is_not_posted_rm_purchase = $rmPurchaseOrderModel->select($selectQry)->first();
         $jumlah_is_not_posted_am_purchase = $amPurchaseOrderModel->select($selectQry)->first();
@@ -40,23 +61,55 @@ class Dashboard extends BaseController
         $jumlah_sales_order_bulan_ini = $salesOrderModel
             ->select("count(*) as total_bulan_ini")
             ->where("DATE_FORMAT(createdAt, '%m')", $month)
+            ->where('id_company', $this->this_company_id)
+            ->where('deletedAt', null)
             ->first();
 
         $jumlah_sales_order_export_bulan_ini = $salesOrderExportModel
             ->select("count(*) as total_bulan_ini")
             ->where("DATE_FORMAT(createdAt, '%m')", $month)
+            ->where($condition)
             ->first();
         $jumlah_sales_order_lain_bulan_ini = $salesOrderLainModel
             ->select("count(*) as total_bulan_ini")
             ->where("DATE_FORMAT(createdAt, '%m')", $month)
+            ->where($condition)
             ->first();
 
         $jumlah_sales_bulan_ini =  $jumlah_sales_order_bulan_ini['total_bulan_ini'] +  $jumlah_sales_order_export_bulan_ini['total_bulan_ini']
             + $jumlah_sales_order_lain_bulan_ini['total_bulan_ini'];
 
+        $jumlah_warehouse_bulan_ini = $warehouseModel
+            ->select("count(*) as total_bulan_ini")
+            ->where("DATE_FORMAT(createdAt, '%m')", $month)
+            ->where($condition)
+            ->first();
+
+        $jumlah_work_orders = $workOrdersModel
+            ->select("count(*) as total_work_orders")
+            ->where($condition)
+            ->first();
+
+        $jumlah_users = $userModel
+            ->select("count(*) as total_user")
+            ->where('current_company_id', $this->this_company_id)
+            ->where('deletedAt', null)
+            ->first();
+
+        $jumlah_customer = $customerModel
+            ->select("count(*) as total_customer")
+            ->where($condition)
+            ->first();
+
+
+
         $data = [
             'jumlah_pembayaran' => $jumlah_pembayaran_belum_posting,
-            'jumlah_sales_bulan_ini' => $jumlah_sales_bulan_ini
+            'jumlah_sales_bulan_ini' => $jumlah_sales_bulan_ini,
+            'jumlah_warehouse_bulan_ini' => $jumlah_warehouse_bulan_ini['total_bulan_ini'],
+            'jumlah_work_orders' => $jumlah_work_orders['total_work_orders'],
+            'jumlah_users' => $jumlah_users['total_user'],
+            'jumlah_customer' => $jumlah_customer['total_customer']
         ];
         return view('Dashboard/dashboard/index', $data);
     }
--- app/Views/Dashboard/dashboard/index.php
+++ app/Views/Dashboard/dashboard/index.php
@@ -12,6 +12,7 @@
         </div>
         <div class="card-body">
             <div class="row" style="margin-top: -20px;">
+
                 <div class="col-sm-1 mr-4 ml-4 mt-3">
                     <div class="btn-group dropend">
                         <button class="btn dropdown-toggle p-3" style="background-color: #E02B35; color: white; width:160px; " type="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -23,11 +24,21 @@
                             <?php endif; ?>
                         </button>
                         <ul class="dropdown-menu dropdown-menu-end">
-                            <li><a class="dropdown-item" href="<?= base_url("spp"); ?>">SPP</a></li>
-                            <li><a class="dropdown-item" href="<?= base_url("po-lokal-bahan-baku"); ?>">PO LOKAL BB </a></li>
-                            <li><a class="dropdown-item" href="<?= base_url("po-lokal-bahan-penolong"); ?>">PO LOKAL BP</a></li>
-                            <li><a class="dropdown-item" href="<?= base_url("po-import-bahan-baku"); ?>">PO IMPORT BB</a></li>
-                            <li><a class="dropdown-item" href="<?= base_url("po-import-bahan-penolong"); ?>">PO IMPORT BP</a></li>
+                            <?php if (can('Pembelian', 'SPP', 'r')) : ?>
+                                <li><a class="dropdown-item" href="<?= base_url("spp"); ?>">SPP</a></li>
+                            <?php endif; ?>
+                            <?php if (can('Pembelian', 'PO Lokal BB', 'r')) : ?>
+                                <li><a class="dropdown-item" href="<?= base_url("po-lokal-bahan-baku"); ?>">PO LOKAL BB </a></li>
+                            <?php endif; ?>
+                            <?php if (can('Pembelian', 'PO Lokal BP', 'r')) : ?>
+                                <li><a class="dropdown-item" href="<?= base_url("po-lokal-bahan-penolong"); ?>">PO LOKAL BP</a></li>
+                            <?php endif; ?>
+                            <?php if (can('Pembelian', 'PO Import BB', 'r')) : ?>
+                                <li><a class="dropdown-item" href="<?= base_url("po-import-bahan-baku"); ?>">PO IMPORT BB</a></li>
+                            <?php endif; ?>
+                            <?php if (can('Pembelian', 'PO Import BP', 'r')) : ?>
+                                <li><a class="dropdown-item" href="<?= base_url("po-import-bahan-penolong"); ?>">PO IMPORT BP</a></li>
+                            <?php endif; ?>
                         </ul>
                     </div>
                 </div>
@@ -42,37 +53,48 @@
                             <?php endif; ?>
                         </button>
                         <ul class="dropdown-menu dropdown-menu-end">
-                            <li><a class="dropdown-item" href="<?= base_url("order-form-lokal"); ?>">PENJUALAN LOKAL</a></li>
-                            <li><a class="dropdown-item" href="<?= base_url("order-form-internasional"); ?>">PENJUALAN EXPOR</a></li>
-                            <li><a class="dropdown-item" href="<?= base_url("order-form-lain"); ?>">PENJUALAN LAIN</a></li>
+                            <?php if (can('Penjualan Lokal', 'Order Form', 'r')) : ?>
+                                <li><a class="dropdown-item" href="<?= base_url("order-form-lokal"); ?>">PENJUALAN LOKAL</a></li>
+                            <?php endif; ?>
+                            <?php if (can('Penjualan Ekspor', 'Order Form', 'r')) : ?>
+                                <li><a class="dropdown-item" href="<?= base_url("order-form-internasional"); ?>">PENJUALAN EKSPOR</a></li>
+                            <?php endif; ?>
+                            <?php if (can('Penjualan Lain', 'Order Form', 'r')) : ?>
+                                <li><a class="dropdown-item" href="<?= base_url("order-form-lain"); ?>">PENJUALAN LAIN</a></li>
+                            <?php endif; ?>
                         </ul>
                     </div>
                 </div>
 
-                <div class="col-sm-1 mr-4 ml-4 mt-3">
-                    <div class="btn-group">
-                        <a href="<?= base_url("laporan-warehouse"); ?>" class="btn p-3" style="background-color: #E02B35; color: white; width:160px">
-                            <i class="fas fa-truck"></i> Material Req.
-                            <?php if (!empty($jumlah_sales_bulan_ini)) : ?>
-                                <span class=" position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
-                                    <?= !empty($jumlah_sales_bulan_ini) ? $jumlah_sales_bulan_ini : "-" ?>
-                                </span>
-                            <?php endif; ?>
-                        </a>
+                <?php if (can('Laporan', 'Warehouse', 'r')) : ?>
+                    <div class="col-sm-1 mr-4 ml-4 mt-3">
+                        <div class="btn-group">
+                            <a href="<?= base_url("laporan-warehouse"); ?>" class="btn p-3" style="background-color: #E02B35; color: white; width:160px">
+                                <i class="fas fa-truck"></i> Material Req.
+                                <?php if (!empty($jumlah_warehouse_bulan_ini)) : ?>
+                                    <span class=" position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
+                                        <?= !empty($jumlah_warehouse_bulan_ini) ? $jumlah_warehouse_bulan_ini : "-" ?>
+                                    </span>
+                                <?php endif; ?>
+                            </a>
+                        </div>
                     </div>
-                </div>
-                <div class="col-sm-1 mr-4 ml-4 mt-3">
-                    <div class="btn-group">
-                        <a href="<?= base_url("work-order"); ?>" class="btn p-3" style="background-color: #E02B35; color: white; width:160px">
-                            <i class="fas fa-play"></i> Production
-                            <?php if (!empty($jumlah_sales_bulan_ini)) : ?>
-                                <span class=" position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
-                                    <?= !empty($jumlah_sales_bulan_ini) ? $jumlah_sales_bulan_ini : "-" ?>
-                                </span>
-                            <?php endif; ?>
-                        </a>
+                <?php endif; ?>
+
+                <?php if (can('Produksi', 'Work Order', 'r')) : ?>
+                    <div class="col-sm-1 mr-4 ml-4 mt-3">
+                        <div class="btn-group">
+                            <a href="<?= base_url("work-order"); ?>" class="btn p-3" style="background-color: #E02B35; color: white; width:160px">
+                                <i class="fas fa-play"></i> Production
+                                <?php if (!empty($jumlah_work_orders)) : ?>
+                                    <span class=" position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
+                                        <?= !empty($jumlah_work_orders) ? $jumlah_work_orders : "-" ?>
+                                    </span>
+                                <?php endif; ?>
+                            </a>
+                        </div>
                     </div>
-                </div>
+                <?php endif; ?>
 
                 <div class="col-sm-1 mr-4 ml-4 mt-3">
                     <div class="btn-group dropend">
@@ -81,43 +103,61 @@
 
                         </button>
                         <ul class="dropdown-menu dropdown-menu-end">
-                            <li><a class="dropdown-item" href="<?= base_url("proses-rebus"); ?>">PROSES REBUS</a></li>
-                            <li><a class="dropdown-item" href="<?= base_url("jasa-vendor-out"); ?>">BARANG KELUAR </a></li>
-                            <li><a class="dropdown-item" href="<?= base_url("jasa-vendor-in"); ?>">BARANG MASUK</a></li>
-                            <li><a class="dropdown-item" href="<?= base_url("biaya-udang"); ?>">BIAYA UDANG</a></li>
-                            <li><a class="dropdown-item" href="<?= base_url('biaya-kepiting'); ?>">BIAYA KEPITING</a></li>
+
+                            <?php if (can('Jasa Vendor', 'Proses Rebus', 'r')) : ?>
+                                <li><a class="dropdown-item" href="<?= base_url("proses-rebus"); ?>">PROSES REBUS</a></li>
+                            <?php endif; ?>
+
+                            <?php if (can('Jasa Vendor', 'Barang Keluar', 'r')) : ?>
+                                <li><a class="dropdown-item" href="<?= base_url("jasa-vendor-out"); ?>">BARANG KELUAR </a></li>
+                            <?php endif; ?>
+
+                            <?php if (can('Jasa Vendor', 'Barang Masuk', 'r')) : ?>
+                                <li><a class="dropdown-item" href="<?= base_url("jasa-vendor-in"); ?>">BARANG MASUK</a></li>
+                            <?php endif; ?>
+
+                            <?php if (can('Jasa Vendor', 'Biaya Udang', 'r')) : ?>
+                                <li><a class="dropdown-item" href="<?= base_url("biaya-udang"); ?>">BIAYA UDANG</a></li>
+                            <?php endif; ?>
+
+                            <?php if (can('Jasa Vendor', 'Biaya Kepiting', 'r')) : ?>
+                                <li><a class="dropdown-item" href="<?= base_url('biaya-kepiting'); ?>">BIAYA KEPITING</a></li>
+                            <?php endif; ?>
                         </ul>
                     </div>
                 </div>
-                <div class="col-sm-1 mr-4 ml-4 mt-3">
-                    <div class="btn-group">
-                        <a href="<?= base_url("kurs"); ?>" class="btn p-3" style="background-color: #E02B35; color: white; width:160px">
-                            <div class="text-center">
-                                <i class="fa-solid fa-dollar-sign fa-lg"></i>
-                                KURS
-                            </div>
+                <?php if (can('Master Data', 'Kurs', 'r')) : ?>
+                    <div class="col-sm-1 mr-4 ml-4 mt-3">
+                        <div class="btn-group">
+                            <a href="<?= base_url("kurs"); ?>" class="btn p-3" style="background-color: #E02B35; color: white; width:160px">
+                                <div class="text-center">
+                                    <i class="fa-solid fa-dollar-sign fa-lg"></i>
+                                    KURS
+                                </div>
 
-                            <?php if (!empty($jumlah_sales_bulan_ini)) : ?>
-                                <span class=" position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
-                                    <?= !empty($jumlah_sales_bulan_ini) ? $jumlah_sales_bulan_ini : "-" ?>
-                                </span>
-                            <?php endif; ?>
-                        </a>
+                                <?php if (!empty($jumlah_sales_bulan_ini)) : ?>
+                                    <span class=" position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
+                                        <?= !empty($jumlah_sales_bulan_ini) ? $jumlah_sales_bulan_ini : "-" ?>
+                                    </span>
+                                <?php endif; ?>
+                            </a>
+                        </div>
                     </div>
-                </div>
-
-                <div class="col-sm-1 mr-4 ml-4 mt-3">
-                    <div class="btn-group">
-                        <a href="<?= base_url("user");  ?>" class="btn p-3" style="background-color: #E02B35; color: white; width:160px">
-                            <i class="fas fa-users"></i> Users
-                            <?php if (!empty($jumlah_sales_bulan_ini)) : ?>
-                                <span class=" position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
-                                    <?= !empty($jumlah_sales_bulan_ini) ? $jumlah_sales_bulan_ini : "-" ?>
-                                </span>
-                            <?php endif; ?>
-                        </a>
+                <?php endif; ?>
+                <?php if (can('Settings', 'Manajemen User', 'r')) : ?>
+                    <div class="col-sm-1 mr-4 ml-4 mt-3">
+                        <div class="btn-group">
+                            <a href="<?= base_url("user");  ?>" class="btn p-3" style="background-color: #E02B35; color: white; width:160px">
+                                <i class="fas fa-users"></i> Users
+                                <?php if (!empty($jumlah_users)) : ?>
+                                    <span class=" position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
+                                        <?= !empty($jumlah_users) ? $jumlah_users : "-" ?>
+                                    </span>
+                                <?php endif; ?>
+                            </a>
+                        </div>
                     </div>
-                </div>
+                <?php endif; ?>
 
                 <div class="col-sm-1 mr-4 ml-4 mt-3">
                     <div class="btn-group dropend">
@@ -128,28 +168,45 @@
                             </span>
                         </button>
                         <ul class="dropdown-menu dropdown-menu-end">
-                            <li><a class="dropdown-item" href="<?= base_url("barang-bahan-baku");  ?>">BAHAN BAKU</a></li>
-                            <li><a class="dropdown-item" href="<?= base_url("barang-bahan-penolong");  ?>">BAHAN PENOLONG</a></li>
-                            <li><a class="dropdown-item" href="<?= base_url("barang-bahan-jadi");  ?>">BAHAN JADI</a></li>
-                            <li><a class="dropdown-item" href="<?= base_url("barang-scrap");  ?>">BAHAN SCRAP</a></li>
-                            <li><a class="dropdown-item" href="<?= base_url("barang-modal");  ?>">BAHAN MODAL</a></li>
-                            <li><a class="dropdown-item" href="<?= base_url("barang-setengah-jadi");  ?>">BAHAN SETENGAH JADI</a></li>
-                            <li><a class="dropdown-item" href="<?= base_url("kemasan");  ?>">KEMANASAN</a></li>
+                            <?php if (can('Master Barang', 'Bahan Baku', 'r')) : ?>
+                                <li><a class="dropdown-item" href="<?= base_url("barang-bahan-baku");  ?>">BAHAN BAKU</a></li>
+                            <?php endif; ?>
+                            <?php if (can('Master Barang', 'Bahan Penolong', 'r')) : ?>
+                                <li><a class="dropdown-item" href="<?= base_url("barang-bahan-penolong");  ?>">BAHAN PENOLONG</a></li>
+                            <?php endif; ?>
+                            <?php if (can('Master Barang', 'Bahan jadi', 'r')) : ?>
+                                <li><a class="dropdown-item" href="<?= base_url("barang-bahan-jadi");  ?>">BAHAN JADI</a></li>
+                            <?php endif; ?>
+                            <?php if (can('Master Barang', 'Barang Scrap', 'r')) : ?>
+                                <li><a class="dropdown-item" href="<?= base_url("barang-scrap");  ?>">BARANG SCRAP</a></li>
+                            <?php endif; ?>
+                            <?php if (can('Master Barang', 'Barang Modal', 'r')) : ?>
+                                <li><a class="dropdown-item" href="<?= base_url("barang-modal");  ?>">BARANG MODAL</a></li>
+                            <?php endif; ?>
+                            <?php if (can('Master Barang', 'Bahan Setengah Jadi', 'r')) : ?>
+                                <li><a class="dropdown-item" href="<?= base_url("barang-setengah-jadi");  ?>">BAHAN SETENGAH JADI</a></li>
+                            <?php endif; ?>
+                            <?php if (can('Master Barang', 'Kemasan', 'r')) : ?>
+                                <li><a class="dropdown-item" href="<?= base_url("kemasan");  ?>">KEMANASAN</a></li>
+                            <?php endif; ?>
                         </ul>
                     </div>
                 </div>
-                <div class="col-sm-1 mr-4 ml-4 mt-3">
-                    <div class="btn-group">
-                        <a href="<?= base_url("customer");  ?>" class="btn p-3" style="background-color: #E02B35; color: white; width:160px">
-                            <i class="fas fa-address-book"></i> Customer
-                            <?php if (!empty($jumlah_sales_bulan_ini)) : ?>
-                                <span class=" position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
-                                    <?= !empty($jumlah_sales_bulan_ini) ? $jumlah_sales_bulan_ini : "-" ?>
-                                </span>
-                            <?php endif; ?>
-                        </a>
+
+                <?php if (can('Master Data', 'Customer Global', 'r')) : ?>
+                    <div class="col-sm-1 mr-4 ml-4 mt-3">
+                        <div class="btn-group">
+                            <a href="<?= base_url("customer");  ?>" class="btn p-3" style="background-color: #E02B35; color: white; width:160px">
+                                <i class="fas fa-address-book"></i> Customer
+                                <?php if (!empty($jumlah_customer)) : ?>
+                                    <span class=" position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
+                                        <?= !empty($jumlah_customer) ? $jumlah_customer : "-" ?>
+                                    </span>
+                                <?php endif; ?>
+                            </a>
+                        </div>
                     </div>
-                </div>
+                <?php endif; ?>
 
 
 
