861b92c0c474c508401fafdddb1bd34d3fb10177
Ardhikarestu <ardhikayoviyanto@gmail.com>
2024-09-04 15:01:55 +0700

    feat(bug ppn po lokal bp)

--- app/Controllers/Purchase/POLokalBahanPenolong.php
+++ app/Controllers/Purchase/POLokalBahanPenolong.php
@@ -555,7 +555,7 @@ class POLokalBahanPenolong extends BaseController
                 foreach ($dataBPLokalDetail as $value) {
                     $no++;
                     $value->no = $no;
-                    $totalan = formatter($value->price, "CURR_TO_INT") * formatter($value->qty, "STR_TO_FLOAT");
+                    $totalan = $value->price * formatter($value->qty, "STR_TO_FLOAT");
                     $value->nilaiPpn = number_format($totalan * (float)$value->ppnValue / 100);
                     $value->nilaiPph = number_format($totalan * (float)$value->pphValue / 100);
                     $totalTambahan += formatter($value->additional_cost, "CURR_TO_INT");
