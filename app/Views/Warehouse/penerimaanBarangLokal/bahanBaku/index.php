673b61996b45c057855ff569c18104612bca31b3
MuhammadAgungPutraNugroho <muhammadagung0708@example.com>
2024-09-05 11:43:55 +0700

    ganti icon return barang

--- app/Views/Warehouse/penerimaanBarangLokal/bahanBaku/index.php
+++ app/Views/Warehouse/penerimaanBarangLokal/bahanBaku/index.php
@@ -193,7 +193,7 @@
                         string = `
                         <div class="mt-0" >
                             <button data-toggle="tooltip" title="Return Out" class="btn btn-success return-out" onclick="" style="box-shadow: none !important;">
-                                <i class="fa fa-undo fa-sm" aria-hidden="true"></i>
+                                <i class="fa fa-truck fa-sm" aria-hidden="true"></i>
                             </button>
                             <?php if (can('Warehouse', 'P. Barang Lokal BB', 'p')) : ?>
                                 <button data-toggle="tooltip" title="Print" class="btn btn-warning btn-print" onclick="print('<?= base_url("penerimaan-barang-lokal-bb/print/"); ?>${id}')" style="box-shadow: none !important;">
--- app/Views/Warehouse/penerimaanBarangLokal/bahanPenolong/index.php
+++ app/Views/Warehouse/penerimaanBarangLokal/bahanPenolong/index.php
@@ -197,7 +197,7 @@
                         string = `
                         <div class="mt-0" style="text-align:center;">
                             <button data-toggle="tooltip" title="Return Out" class="btn btn-success return-out" onclick="" style="box-shadow: none !important;">
-                                <i class="fa fa-undo fa-sm" aria-hidden="true"></i>
+                                <i class="fa fa-truck fa-sm" aria-hidden="true"></i>
                             </button>
                             <?php if (can('Warehouse', 'P. Barang Lokal BP', 'p')) : ?>
                                 <button data-toggle="tooltip" title="Print" class="btn btn-warning btn-print" onclick="print('<?= base_url("penerimaan-barang-lokal-bp/print/"); ?>${id}')" style="box-shadow: none !important;">
