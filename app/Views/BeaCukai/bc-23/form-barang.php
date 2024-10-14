7ed1483c208428bbf934bb7c3451c7cdd387668b
Ardhikarestu <ardhikayoviyanto@gmail.com>
2024-10-14 13:18:05 +0700

    update 4 angka dibelakang koma di modul bc

--- app/Views/BeaCukai/bc-23/form-barang.php
+++ app/Views/BeaCukai/bc-23/form-barang.php
@@ -70,8 +70,8 @@
                                     <td style="text-align: center;"><?= $l['po_no'] ?></td>
                                     <td style="text-align: center;"><?= $l['kode_barang'] ?></td>
                                     <td style="text-align: center;"><?= $l['barang_name'] ?></td>
-                                    <td style="text-align: center;"><?= number_format($l['qty_po'], 2) ?></td>
-                                    <td style="text-align: center;"><?= number_format($l['qty_lpb'], 2) ?></td>
+                                    <td style="text-align: center;"><?= number_format($l['qty_po'], 4) ?></td>
+                                    <td style="text-align: center;"><?= number_format($l['qty_lpb'], 4) ?></td>
                                     <td style="text-align: center;"><?= str_replace('Rp', '', toRupiah($l['harga'])) ?></td>
                                     <td style="text-align: center;" class="body-table-info-status-barang-root-view" data-id="<?= encrypt($l['penerimaan_barang_id']) ?>">
                                         <?php if ($bcDokumenBarang == null) : ?>
