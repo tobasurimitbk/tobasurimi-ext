cb3d7f9952cd850bdda9aa0826c78cb067a14301
Ardhikarestu <ardhikayoviyanto@gmail.com>
2025-01-14 03:12:21 -0500

    update no

--- app/Models/PenerimaanBarangModel.php
+++ app/Models/PenerimaanBarangModel.php
@@ -316,7 +316,7 @@ class PenerimaanBarangModel extends Model
         $builder = $this->db->table('penerimaan_barang');
         $builder->select('no_penerimaan_barang');
         $builder->orderBy('no_penerimaan_barang', 'desc');
-        $builder->where('penerimaan_barang.warehouse_id', $warehouseID);
+        // $builder->where('penerimaan_barang.warehouse_id', $warehouseID);
         $builder->where('createdAt >=', $thn . "-" . $bln . "-01" . " 00:00:00")
             ->where('createdAt <=', $last_day . " 23:59:59");
         $builder->like('no_penerimaan_barang', $lastStr);
