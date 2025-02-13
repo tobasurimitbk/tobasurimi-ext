b8bb720ee8b1ca6ac04b05a3d9b99b46daf16985
AgungPutra0708 <muhammadagung0708@gmail.com>
2025-02-13 11:51:28 +0700

    fix error di account barang

--- app/Models/AccountBarangModel.php
+++ app/Models/AccountBarangModel.php
@@ -75,10 +75,12 @@ class AccountBarangModel extends Model
         $selectQry = "barang_master.kode_barang,
                   barang_master.barang_name,
                   account_barang.id,
+                  account_barang.divisi_id,
                   account_barang.ap_id,
                   account_barang.ar_id,
                   account_barang.pemakaian_id,
                   account_barang.kategori_id,
+                  account_barang.keterangan,
                   divisis.divisi,
                   barang_master_spesifikasi.spesifikasi";
 
