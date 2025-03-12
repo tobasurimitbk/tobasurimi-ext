d3474e4c920aeff4cebc06f9d85c49d45f2dd539
rianmaulana <rian87334@gmail.com>
2025-03-12 10:31:57 +0700

    update

--- app/Controllers/Inventori/StokAdjusment.php
+++ app/Controllers/Inventori/StokAdjusment.php
@@ -441,7 +441,7 @@ class StokAdjusment extends BaseController
             for ($i = 0; $i < count($dataResult); $i++) {
                 $bcType = $this->metaDataModel->find($dataResult[$i]['bc_id']);
 
-                if ($bcType == "NON PABEAN") {
+                if ($bcType != "PABEAN") {
                     // NON PABEAN
                     $noDaftar = "-";
                 } elseif ($dataResult[$i]['bc_id'] == 48) {
