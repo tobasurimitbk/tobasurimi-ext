a6d23f6e1ed95601237e5dd2db7785ecdfb0899e
AgungPutra0708 <muhammadagung0708@gmail.com>
2025-03-13 17:06:59 +0700

    fix

--- app/Controllers/Accounting/AccountSupplier/AccountSupplierController.php
+++ app/Controllers/Accounting/AccountSupplier/AccountSupplierController.php
@@ -66,7 +66,7 @@ class AccountSupplierController extends BaseController
         if ($this->is_admin == 1) {
             $condition = [
                 "suppliers.company_id"  => $this->this_company_id,
-                "account_supplier.company_id"  => $this->this_company_id,
+                // "account_supplier.company_id"  => $this->this_company_id,
                 "suppliers.deletedAt" => NULL
             ];
         } else {
