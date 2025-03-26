c5f5dd496d50d54a13cc0a0cfb2498d4ee8ef676
rianmaulana <rian87334@gmail.com>
2025-03-26 11:37:59 +0700

    update

--- app/Views/SalesLokal/OrderForm/index.php
+++ app/Views/SalesLokal/OrderForm/index.php
@@ -334,12 +334,8 @@
                 data: "nama_customer",
                 className: "text-center"
             }, {
-                data: "customer_name",
-                className: "text-center",
-                render: function(data, type, row) {
-                    // Jika customer_name tidak ada (null/undefined/empty), tampilkan "DATA IMPORT"
-                    return data ? data : 'DATA IMPORT';
-                }
+                data: "destination",
+                className: "text-center"
             }, {
                 data: "qty_barang",
                 className: "text-center"
