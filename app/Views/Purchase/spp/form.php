e52750ceed30641e16a4e68aa613aed390628dda
Ardhikarestu <ardhikayoviyanto@gmail.com>
2025-03-28 10:40:36 +0700

    feat(typo)

--- app/Views/Purchase/spp/form.php
+++ app/Views/Purchase/spp/form.php
@@ -2237,7 +2237,7 @@
                 'nama_satuan': "<?= $d->kode_satuan ?>",
                 'satuan_id': "<?= $d->unit ?>",
                 'qty': "<?= $d->qty ?>",
-                'keterangan': "<?= json_encode(trim($d->note)) ?>"
+                'keterangan': "<?= str_replace('"', '\"', $d->note)  ?>"
             });
         <?php endforeach; ?>
         drawTableDetail();
