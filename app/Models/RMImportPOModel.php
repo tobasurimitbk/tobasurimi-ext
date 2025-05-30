81ea74a6c882bf2489e4dbb2fadfd61c67cf6280
Ardhikarestu <ardhikayoviyanto@gmail.com>
2025-05-30 13:34:23 +0700

    feat(typo)

--- app/Models/RMImportPOModel.php
+++ app/Models/RMImportPOModel.php
@@ -109,7 +109,8 @@ class RMImportPOModel extends Model
         }
 
         if ($addCondition['search']) {
-            $poDataQry->like('rm_import_pos.po_no', $addCondition['search']);
+            $poDataQry->like('rm_import_pos.po_no', $addCondition['search'])
+                ->orLike('suppliers.name', $addCondition['search']);
         }
 
         if ($addCondition['dateStart']) {
