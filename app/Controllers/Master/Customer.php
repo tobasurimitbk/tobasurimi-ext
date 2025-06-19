89c1c3c49d6337f07158303fe25fe5e87e7f148e
Ardhikarestu <ardhikayoviyanto@gmail.com>
2025-06-19 15:00:06 +0700

    feat(typo)

--- app/Controllers/Master/Customer.php
+++ app/Controllers/Master/Customer.php
@@ -325,7 +325,7 @@ class Customer extends BaseController
                 $values = [
                     "company_id" => $this->this_company_id,
                     "user_id" => $this->this_user_id,
-                    // "kode" => $this->request->getPost("kode"),
+                    "kode" => $this->request->getPost("kode"),
                     "name" => strtoupper($this->request->getVar("name")),
                     "address" => strtoupper($this->request->getVar("address")),
                     "nik" => $this->request->getPost("nik"),
