adc24137b6554e5459de752720c83aafc8f83ff4
AgungPutra0708 <muhammadagung0708@gmail.com>
2025-07-01 14:34:45 +0700

    fix command dd

--- app/Controllers/Laporan/Accounting/Hutang.php
+++ app/Controllers/Laporan/Accounting/Hutang.php
@@ -125,7 +125,8 @@ class Hutang extends BaseController
             "lastdate"      => $this->request->getVar("dateEnd") ? date("Y-m-d", strtotime(str_replace("/", "-", $this->request->getVar("dateEnd")))) : "",
         ];
 
-        dd($this->this_company_id);
+        // dd($this->this_company_id);
+
 
         if ($this->this_company_id != "16" && $this->this_company_id != "15") {
             $companyId = [1, 2];
