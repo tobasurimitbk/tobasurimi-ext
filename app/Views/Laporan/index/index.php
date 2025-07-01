bc0bdc416cc857c8868bd77ab27acad8ae52899b
AgungPutra0708 <muhammadagung0708@gmail.com>
2025-07-01 15:37:59 +0700

    hide neraca neraca saldo laba rugi costing kopek

--- app/Views/Laporan/index/index.php
+++ app/Views/Laporan/index/index.php
@@ -90,7 +90,7 @@
                 </div>
             </a>
         </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <!-- <div class="col-lg-3 col-md-6 col-sm-6 col-12">
             <a href="<?= base_url('laporan-accounting/neracasaldo') ?>">
                 <div class="card card-statistic-1">
                     <div class="card-icon bg-danger">
@@ -103,8 +103,8 @@
                     </div>
                 </div>
             </a>
-        </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        </div> -->
+        <!-- <div class="col-lg-3 col-md-6 col-sm-6 col-12">
             <a href="<?= base_url('laporan-accounting/labarugi') ?>">
                 <div class="card card-statistic-1">
                     <div class="card-icon bg-danger">
@@ -131,7 +131,7 @@
                     </div>
                 </div>
             </a>
-        </div>
+        </div> -->
         <!-- <div class="col-lg-3 col-md-6 col-sm-6 col-12">
             <div class="card card-statistic-1">
                 <div class="card-icon bg-danger">
@@ -144,7 +144,7 @@
                 </div>
             </div>
         </div> -->
-        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <!-- <div class="col-lg-3 col-md-6 col-sm-6 col-12">
             <a href="<?= base_url('laporan-accounting/costing') ?>">
                 <div class="card card-statistic-1">
                     <div class="card-icon bg-danger">
@@ -171,7 +171,7 @@
                     </div>
                 </div>
             </a>
-        </div>
+        </div> -->
         <!-- <div class="col-lg-3 col-md-6 col-sm-6 col-12">
             <div class="card card-statistic-1">
                 <div class="card-icon bg-danger">
