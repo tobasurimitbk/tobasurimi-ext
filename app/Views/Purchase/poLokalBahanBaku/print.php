558aad13ddc1d6436be30c537ba092ad79c1410f
AgungPutra0708 <muhammadagung0708@gmail.com>
2025-07-09 14:58:19 +0700

    fix terbilang

--- app/Views/Purchase/poLokalBahanBaku/print.php
+++ app/Views/Purchase/poLokalBahanBaku/print.php
@@ -423,9 +423,9 @@
                     <td style="vertical-align: top;">BANYAKNYA UANG (AMOUNT)</td>
                     <td style="vertical-align: top;">: </td>
                     <?php if ($dataPO->pph === "Company") { ?>
-                        <td style="vertical-align: top;"><?= strtoupper(terbilang(formatter(round($nilai_total - ($nilai_total * $dataPO->nilai_pph2)), "STR_TO_FLOAT"))) ?></td>
+                        <td style="vertical-align: top;"><?= strtoupper(terbilang(formatter(($nilai_total - ($nilai_total * $dataPO->nilai_pph2)), "STR_TO_FLOAT"))) ?></td>
                     <?php } else if ($dataPO->pph === "Supplier") { ?>
-                        <td style="vertical-align: top;"><?= strtoupper(terbilang(formatter(round($nilai_total - ($nilai_total * $dataPO->nilai_pph2)), "STR_TO_FLOAT"))) ?></td>
+                        <td style="vertical-align: top;"><?= strtoupper(terbilang(formatter(($nilai_total - ($nilai_total * $dataPO->nilai_pph2)), "STR_TO_FLOAT"))) ?></td>
                     <?php } else { ?> <td style="vertical-align: top;"><?= strtoupper(terbilang(formatter(($nilai_total), "STR_TO_FLOAT"))) ?></td>
                     <?php } ?>
                 </tr>
