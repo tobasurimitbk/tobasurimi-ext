552127ea4f2adb230b27fe144a745b263ced7215
rianmaulana <rian87334@gmail.com>
2025-07-23 13:46:26 +0700

    update

--- app/Views/Pembayaran/pembayaranPanjarSupplier/index.php
+++ app/Views/Pembayaran/pembayaranPanjarSupplier/index.php
@@ -1240,9 +1240,12 @@
             $(".title-name").text("Tambah Data Panjar & Pinjaman");
             $(".delete-btn").css('display', 'none');
             $(".add-modal").modal("show");
-            initSelect2()
             $(".btn-submit-form").show();
             $(".create-form input, .create-form select, .btn-add-detail").prop("disabled", false);
+        });
+
+        $('.add-modal').on('shown.bs.modal', function() {
+            initSelect2();
         });
 
         // Handle submit form
