921ffd08a3cf7e07608a3f370eded756ff03ed02
Ardhikarestu <ardhikayoviyanto@gmail.com>
2025-07-29 14:58:22 +0700

    update

--- app/Controllers/SalesInternasional/SalesKontrak.php
+++ app/Controllers/SalesInternasional/SalesKontrak.php
@@ -598,7 +598,7 @@ class SalesKontrak extends BaseController
 
         $this->salesKontrakDetailModel->where('sales_contract_id', $id)->whereNotIn('id', $id_detail_all)->delete();
         $this->salesContractSizeBreakdownModel->whereNotIn('id', $id_size_breakdown)->delete();
-        $this->salesOrderExportDetailModel->whereNotIn('sales_contract_size_breakdown_id', $id_size_breakdown)->delete();
+        // $this->salesOrderExportDetailModel->whereNotIn('sales_contract_size_breakdown_id', $id_size_breakdown)->delete();
 
         return response()->setJSON([
             'message' => "Sales Kontrak Updated",
