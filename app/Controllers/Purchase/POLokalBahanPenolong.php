2ab77b358c5bed781145b281a565489f55e4f039
ardhikayoviyanto <ardhikayoviyanto@gmail.com>
2025-09-18 22:22:35 +0700

    hapus round

--- app/Controllers/Purchase/POLokalBahanPenolong.php
+++ app/Controllers/Purchase/POLokalBahanPenolong.php
@@ -733,11 +733,11 @@ class POLokalBahanPenolong extends BaseController
                     $keterangan[] = $value->note;
                 }
 
-                $dataBPLokal->totalTambahan = number_format(formatter(round($totalTambahan), "STR_TO_FLOAT"), 2, '.', ',');
-                $dataBPLokal->totalPrice = number_format(formatter(round($totalPrice), "STR_TO_FLOAT"), 2, '.', ',');
-                $dataBPLokal->totalDisc = number_format(formatter(round($totalDisc), "STR_TO_FLOAT"), 2, '.', ',');
-                $dataBPLokal->totalPpn = number_format(formatter(round($totalPpn), "STR_TO_FLOAT"), 2, '.', ',');
-                $dataBPLokal->totalPo = number_format(formatter(round($totalTambahan + $totalPrice - $totalDisc + $totalPpn), "STR_TO_FLOAT"), 2, '.', ',');
+                $dataBPLokal->totalTambahan = number_format(formatter(($totalTambahan), "STR_TO_FLOAT"), 2, '.', ',');
+                $dataBPLokal->totalPrice = number_format(formatter(($totalPrice), "STR_TO_FLOAT"), 2, '.', ',');
+                $dataBPLokal->totalDisc = number_format(formatter(($totalDisc), "STR_TO_FLOAT"), 2, '.', ',');
+                $dataBPLokal->totalPpn = number_format(formatter(($totalPpn), "STR_TO_FLOAT"), 2, '.', ',');
+                $dataBPLokal->totalPo = number_format(formatter(($totalTambahan + $totalPrice - $totalDisc + $totalPpn), "STR_TO_FLOAT"), 2, '.', ',');
                 $dataBPLokal->keterangan = implode(",", array_unique($keterangan));
                 $dataBPLokal->jatuhTempoHari = \totalDayInRange($dataBPLokal->po_date, $dataBPLokal->payment_date);
                 $data["dataPOLokal"] = $dataBPLokal;
