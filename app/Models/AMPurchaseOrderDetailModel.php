fc8572c3fd8fa6c4dfc8e0e377e2dfdc48bd2b14
ardhikayoviyanto <ardhikayoviyanto@gmail.com>
2025-09-18 21:36:43 +0700

    update lpb

--- app/Models/AMPurchaseOrderDetailModel.php
+++ app/Models/AMPurchaseOrderDetailModel.php
@@ -306,7 +306,7 @@ class AMPurchaseOrderDetailModel extends Model
                         'jml_diterima_total' => $jmlMasukAll,
                         'sisa_total' => round($sisaDiterima, 4),
                         'harga' => $harga,
-                        'sub_total' => round($inLPB * $harga),
+                        'sub_total' => isset($firstLPB) ? (float)$firstLPB['sub_total'] : round($inLPB * $harga),
                         'keterangan' => $b['note'],
                         // TAMBAHAN
                         'satuan_id' => $b['unit'],
@@ -347,7 +347,7 @@ class AMPurchaseOrderDetailModel extends Model
                         'jml_diterima_total' => $jmlMasukAll,
                         'sisa_total' => round($sisaDiterima, 4),
                         'harga' => $harga,
-                        'sub_total' => round($inLPB * $harga),
+                        'sub_total' => isset($firstLPB) ? (float)$firstLPB['sub_total'] : round($inLPB * $harga),
                         'keterangan' => $b['note'],
                         // TAMBAHAN
                         'satuan_id' => $b['unit'],
