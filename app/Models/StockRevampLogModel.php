1028e82857b99c6d6418d5071f03401424ebe24b
rianmaulana <rian87334@gmail.com>
2025-10-04 16:30:03 +0700

    update

--- app/Models/StockRevampLogModel.php
+++ app/Models/StockRevampLogModel.php
@@ -17,7 +17,7 @@ class StockRevampLogModel extends Model
     protected $allowedFields    = [];
 
     // Dates
-    protected $useTimestamps = false;
+    protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'createdAt';
     protected $updatedField  = 'updatedAt';
