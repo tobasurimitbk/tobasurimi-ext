9ec5677ab37dcf67772c55baf4c8912c61d2d7cb
Rianmaulana86 <rian87334@gmail.com>
2025-10-13 09:01:15 +0700

    update

--- app/Controllers/JasaVendor/JasaVendorOut.php
+++ app/Controllers/JasaVendor/JasaVendorOut.php
@@ -814,6 +814,7 @@ class JasaVendorOut extends BaseController
                 $condition = [
                     'rm_purchase_orders.supplier_id' => $supplierId,
                     'stock_revamp.barang_master_id' => $barangMasterId,
+                    'stock_revamp_detail.qty_diterima <' => 0
                 ];
                 $dataResult = $stockRevampDetailModel->getStockListWithAddConditionForJasaVendorOut($condition);
 
