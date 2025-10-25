657b3fd86cd2750ddf83225f682948dcf069bcbc
ardhikayoviyanto <ardhikayoviyanto@gmail.com>
2025-10-25 11:38:14 +0700

    update ppn pengurang masuk ke totalan

--- app/Views/Purchase/poLokalBahanPenolong/form.php
+++ app/Views/Purchase/poLokalBahanPenolong/form.php
@@ -498,44 +498,47 @@
     });
 
     // HARGA SATUAN DAN QTY CHANE
-    $('#harga_satuan,#qty,#diskon').keyup(function() {
+    $('#harga_satuan,#qty,#diskon,#biaya_tambahan').keyup(function() {
         var hargaSatuan = parseFloat(destroyFormatRupiah($('#harga_satuan').val())) || 0;
         var qty = parseFloat($('#qty').val()) || 1;
-        // var biayaTambahan = parseFloat(destroyFormatRupiah($('#biaya_tambahan').val())) || 0;
+        var biayaTambahan = parseFloat(destroyFormatRupiah($('#biaya_tambahan').val())) || 0;
         var diskon = parseFloat($('#diskon').val()) || 0;
         var diskonHarga = (diskon / 100) * (hargaSatuan * qty);
         var additionalCostType = $('#additional_cost_type option:selected').val();
 
-        // if (additionalCostType == '') {
-        //     biayaTambahan = 0;
-        // } else if (additionalCostType == "MINUS") {
-        //     biayaTambahan = biayaTambahan * -1;
-        // }
+        if (additionalCostType == '' || additionalCostType == 'PLUS') {
+            biayaTambahan = 0;
+        } else if (additionalCostType == "MINUS") {
+            biayaTambahan = biayaTambahan * -1;
+        }
 
-        var total = (((hargaSatuan * qty) - diskonHarga));
+        var total = (((hargaSatuan * qty) - diskonHarga)) + biayaTambahan;
         $('#total').val(total == 0 ? '' : greatFormatRupiah(total.toFixed(2)));
     });
 
-    // $('#additional_cost_type').change(function() {
-    //     var additionalCostType = $('#additional_cost_type option:selected').val();
-    //     var biayaTambahan = parseFloat(destroyFormatRupiah($('#biaya_tambahan').val())) || 0;
-    //     if (additionalCostType == '') {
-    //         $('#biaya_tambahan').val('');
-    //         $('#biaya_tambahan').keyup();
-    //     } else {
-    //         if (additionalCostType == "MINUS") {
-    //             biayaTambahan = biayaTambahan * -1;
-    //         }
-    //         // Hitung Total
-    //         var hargaSatuan = parseFloat(destroyFormatRupiah($('#harga_satuan').val())) || 0;
-    //         var qty = parseFloat($('#qty').val()) || 1;
-    //         var diskon = parseFloat($('#diskon').val()) || 0;
-    //         var diskonHarga = (diskon / 100) * (hargaSatuan * qty);
+    $('#additional_cost_type').change(function() {
+        var additionalCostType = $('#additional_cost_type option:selected').val();
+        if (additionalCostType == '') {
+            $('#biaya_tambahan').val('');
+            $('#biaya_tambahan').keyup();
+        } else {
+            var hargaSatuan = parseFloat(destroyFormatRupiah($('#harga_satuan').val())) || 0;
+            var qty = parseFloat($('#qty').val()) || 1;
+            var biayaTambahan = parseFloat(destroyFormatRupiah($('#biaya_tambahan').val())) || 0;
+            var diskon = parseFloat($('#diskon').val()) || 0;
+            var diskonHarga = (diskon / 100) * (hargaSatuan * qty);
+            var additionalCostType = $('#additional_cost_type option:selected').val();
 
-    //         var total = (((hargaSatuan * qty) - diskonHarga) + biayaTambahan);
-    //         $('#total').val(total == 0 ? '' : greatFormatRupiah(total.toFixed(2)));
-    //     }
-    // });
+            if (additionalCostType == '' || additionalCostType == 'PLUS') {
+                biayaTambahan = 0;
+            } else if (additionalCostType == "MINUS") {
+                biayaTambahan = biayaTambahan * -1;
+            }
+
+            var total = (((hargaSatuan * qty) - diskonHarga)) + biayaTambahan;
+            $('#total').val(total == 0 ? '' : greatFormatRupiah(total.toFixed(2)));
+        }
+    });
 
     // CHANGE TOTAL
     $('#total').keyup(function() {
