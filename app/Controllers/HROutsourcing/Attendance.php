68bc598aa677d56fa28eedbb7bab72740f57a31d
Rianmaulana86 <rian87334@gmail.com>
2025-12-11 16:01:01 +0700

    update

--- app/Controllers/HROutsourcing/Attendance.php
+++ app/Controllers/HROutsourcing/Attendance.php
@@ -50,7 +50,7 @@ class Attendance extends BaseController
             "company" => $this->hrOutSourcingCompanyModel->where('deletedAt', NULL)->findAll(),
         ];
        
-        return view('HROutsourcing/AttendancesUnit/index', $data);
+        return view('HROutsourcing/Attendance/index', $data);
     }
 
     public function allData()
