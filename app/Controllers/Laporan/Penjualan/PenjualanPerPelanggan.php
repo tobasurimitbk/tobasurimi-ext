b151c1934f334e7fb8f4b18c82217c81c4941f5d
AgungPutra0708 <muhammadagung0708@gmail.com>
2025-12-26 16:08:42 +0700

    fix penjualan per pelanggan jenis penjualan

--- app/Controllers/Laporan/Penjualan/PenjualanPerPelanggan.php
+++ app/Controllers/Laporan/Penjualan/PenjualanPerPelanggan.php
@@ -72,9 +72,9 @@ class PenjualanPerPelanggan extends BaseController
             if ($data->jenis_penjualan == 1) {
                 $namaPenjual = $data->salesName;
             } elseif ($data->jenis_penjualan == 2) {
-                $namaPenjual = "E-Commerce";
-            } else {
                 $namaPenjual = "Office";
+            } else {
+                $namaPenjual = "E-Commerce";
             }
             array_push($dataAllSalesOrderInvoice, [
                 "no" => $no++,
