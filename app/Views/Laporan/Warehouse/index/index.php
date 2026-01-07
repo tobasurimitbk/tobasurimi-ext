0da36d1cd5958b85842082fec5c026d64a57b2ef
ardhikayoviyanto <ardhikayoviyanto@gmail.com>
2026-01-07 16:39:36 +0700

    tambah keyup formatrupiah di lpb

--- app/Views/Laporan/Warehouse/index/index.php
+++ app/Views/Laporan/Warehouse/index/index.php
@@ -93,7 +93,7 @@
                 </div>
             </a>
         </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <!-- <div class="col-lg-3 col-md-6 col-sm-6 col-12">
             <a href="<?= base_url('/laporan-warehouse/stock-kartu') ?>">
                 <div class="card card-statistic-1">
                     <div class="card-icon bg-danger">
@@ -106,7 +106,7 @@
                     </div>
                 </div>
             </a>
-        </div>
+        </div> -->
 
     </div>
 
